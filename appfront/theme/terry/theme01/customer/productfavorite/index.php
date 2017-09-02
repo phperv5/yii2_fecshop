4b762eb17257cb2618f1b39ef57d4bb1fdafc2de
iwisunny <iwisunny@gmail.com>
2017-09-02 15:41:15 +0800

    commit

--- appfront/theme/terry/theme01/customer/productfavorite/index.php
+++ appfront/theme/terry/theme01/customer/productfavorite/index.php
@@ -1,51 +1,11 @@
 <div class="main">
-    <div class="main_left">
-
-        <div class="col_d_t">My Account</div>
-        <div class="col_d_m">
-            <div class="blank5px"></div>
-
-            <div class="ml_dir"><a href="orderList.asp">View My Orders</a></div>
-            <div class="ml_dir"><a href="accountSet.asp">Account Settings</a></div>
-            <div class="ml_dir"><a href="addressBook.asp">Manage Address Book</a></div>
-            <div class="ml_dir"><a href="msg_list.asp">Tickets Center</a></div>
-            <div class="ml_dir"><a href="msg_add.asp?DirID=2">Submit a Ticket</a></div>
-
-            <div class="ml_dir"><a href="pro_rev_list.asp">My Products' Reviews</a></div>
-
-
-            <div class="ml_dir ml_dir_cur"><a href="my_favorites_list.asp">My Favorites (<b class="green">2</b>)</a></div>
-
-
-            <div class="blank5px"></div>
-        </div>
-        <div class="col_d_b"></div>
-
-        <div class="blank10px"></div>
-
-
-
-        <div class="fun_column px11 gray word_wrap word_break">
-            <div class="clear"></div>
-            <b class="px14">Welcome!</b><br />
-            <b class="px12">&nbsp;zewe&nbsp;jewew</b><br />
-            User ID: 312043814@qq.com<br />
-            E-Mail: 312043814@qq.com<br />
-            Account Status:<b>Normal</b><br />
-
-            <div class="dashed5px"></div>
-            <div class="align_right"><a href="../members/?action=SignOut"><img src="../images/ico/logout.gif" hspace="5" border="0" align="absmiddle" />Sign out</a>&nbsp;&nbsp;</div>
-            <div class="clear"></div>
-        </div>
-
-        <div class="blank10px"></div>
-
-
-        <div class="fun_column px11 gray">
-            <b class="px14">Need help?</b><br />
-            If you have questions or need help with your account, you may goto "<a href="../support" target="_blank">Help</a>" or <a href="../info/?dirid=8" target="_blank">contact us</a> to assist you.
-            <div class="clear"></div>
-        </div></div>
+    <?php
+    $leftMenu = [
+        'class' => 'fecshop\app\appfront\modules\Customer\block\LeftMenu',
+        'view'	=> 'customer/leftmenu.php'
+    ];
+    ?>
+    <?= Yii::$service->page->widget->render($leftMenu,$this); ?>
     <div class="main_scene">
         <div class="exh_top"></div>
         <div class="exh_main">
@@ -64,9 +24,12 @@
                     <td>Remark</td>
                     <td>Remark</td>
                 </tr>
-
+                <?php if(is_array($coll) && !empty($coll)):  ?>
+                    <?php  foreach($coll as $one):  ?>
+                        <?php var_dump($one);?>
+                        <?php  $main_img = $one['image']['main']['image'];  ?>
                 <tr class="tr_info">
-                    <td class="px11">1</td>
+                    <td class="px11"><?= Yii::$service->url->getUrl($one['url_key'])  ?></td>
                     <td width="70"><div class="img60px"><a href="/wholesale/yanhua-bmw-fem-key-programmer.html" target="_blank"><img src="/upload/pro/yanhua-bmw-fem-key-programmer-180.1.jpg" width="60" height="60" border="0" hspace="0" vspace="0" alt="" align="absmiddle" /></a></div></td>
                     <td class="align_left">
                         <span class="pro_t_newrelease"></span> <a href="/wholesale/yanhua-bmw-fem-key-programmer.html" target="_blank">2017 Latest Yanhua BMW FEM/BDC Key Programmer</a><br /><span class="px11 gray_dark">Item No.SK226</span> 　　<span class="green verdana px10">In Stock</span>
@@ -94,56 +57,18 @@
                         <div class="blank5px"></div>
                         <a href="javascript:void(0);" onclick="MyFavoritesDelete(71861);return false;" title="Delet it from My Favorites">Delete</a></td>
                 </tr>
-
-                <tr class="tr_info">
-                    <td class="px11">2</td>
-                    <td width="70"><div class="img60px"><a href="/wholesale/cgdi-pro-bmw-msv80-key-programmer.html" target="_blank"><img src="/upload/pro/cgdi-pro-bmw-msv80-key-programmer-180.1.jpg" width="60" height="60" border="0" hspace="0" vspace="0" alt="" align="absmiddle" /></a></div></td>
-                    <td class="align_left">
-                        <span class="pro_t_newrelease"></span> <a href="/wholesale/cgdi-pro-bmw-msv80-key-programmer.html" target="_blank">2017 New CGDI Prog BMW MSV80 Auto key programmer + Diagnosis tool+ IMMO Security 3 in 1</a><br /><span class="px11 gray_dark">Item No.SK222</span> 　　<span class="green verdana px10">In Stock</span>
-                    </td>
-                    <td class="align_left">
-                        <span class="pro_pri_curr_vip_s" name="cc_v_USD" style="display:">$489.00</span>
-                        <span class="pro_pri_curr_vip_s" name="cc_v_EUR" style="display:none;">&euro;415.65</span>
-                        <span class="pro_pri_curr_vip_s" name="cc_v_GBP" style="display:none;">&pound;381.42</span>
-                        <span class="pro_pri_curr_vip_s" name="cc_v_AUD" style="display:none;">AU$625.92</span>
-                        <span class="pro_pri_curr_vip_s" name="cc_v_JPY" style="display:none;">&yen;54,279</span>
-                        <span class="pro_pri_off_s" name="cc_v_USD" style="display:">2% off</span>
-                        <span class="pro_pri_off_s" name="cc_v_EUR" style="display:none;">2% off</span>
-                        <span class="pro_pri_off_s" name="cc_v_GBP" style="display:none;">2% off</span>
-                        <span class="pro_pri_off_s" name="cc_v_AUD" style="display:none;">2% off</span>
-                        <span class="pro_pri_off_s" name="cc_v_JPY" style="display:none;">2% off</span>
-
-                    </td>
-                    <td class="align_left px11">
-                        Weight:0.6KG<br />
-                        Package:0*0*0cm
-                        <br /><span class="gray_dark">(Inch: 0*0*0)</span>
-                    </td>
-                    <td class="align_left px11">
-                        Sold: 10<br />
-                        Diggs: 0
-                    </td>
-                    <td class="px10 gray">
-                        <div class="blank5px"></div>
-                        <input name="btn_buyitnow" type="button" class="btn_buyitnow_s_mem" value="" title="Buy It Now" onclick="javascript:ShoppingCartAdd('70841','BuyItNow','N','oSize','N','oColor','N','oQty');return false;" onmouseover="javascript:IsOrderNeedSize('N','oSize');IsOrderNeedColor('N','oColor');IsOrderNeedQty('N','oQty');return false;" />
-                        <div class="blank5px"></div>
-                        <a href="javascript:void(0);" onclick="MyFavoritesDelete(70841);return false;" title="Delet it from My Favorites">Delete</a></td>
-                </tr>
-
+                        <?php  endforeach;  ?>
+                <?php endif; ?>
             </table>
 
 
-            <div class="page_nav"><form onsubmit="location.replace(&quot?ListPager=9284838,2,&quot+this.PageNumber.value);return false">
-                    <div class="fr">
-                        <span>1</span>
+            <div class="page_nav">
+                <?php if($pageToolBar): ?>
+                    <div class="pageToolbar">
+                        <?= $pageToolBar ?>
                     </div>
-                    <div class="fl">
-                        Total:2 items,&nbsp;20 items/p,&nbsp; Page:<b class=red>1</b>/1,&nbsp;Goto<input type="text" class="line" size=2 name="PageNumber" value="1" /><input align="absmiddle" alt="Go!" type="image" name="imageFieldGoto" src="/images/btn/btn_goIcon.gif">
-                    </div><div class=clear></div>
-                </form></div>
-
-
-
+                <?php endif; ?></div>
+            </div>
             <form method="post" name="formOrderAdd" id="formOrderAdd" action="../app/order.asp"><input type="hidden" name="ProID" value="" /><input type="hidden" name="oSize" value="" /><input type="hidden" name="oColor" value="" /><input type="hidden" name="oQty" value="" /><input type="hidden" name="Action" value="" /><input type="hidden" name="AddMethod" value="" /><input type="submit" style="display:none" /></form>
 
             <form name="formMyFavoritesDelete" method="post" action="my_favorites_app.asp">
