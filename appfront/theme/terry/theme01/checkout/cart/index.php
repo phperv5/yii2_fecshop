8e5aa1a1b4298a399f186445c85e88a16f63cae3
iwisunny <iwisunny@gmail.com>
2017-09-02 19:06:28 +0800

    修改购物车

--- appfront/theme/terry/theme01/checkout/cart/index.php
+++ appfront/theme/terry/theme01/checkout/cart/index.php
@@ -1,4 +1,6 @@
-
+<?php
+use fecshop\app\appfront\helper\Format;
+?>
 <div class="main">
     <div class="page_where_l"><a href="../">Home</a> - Shopping Cart</div><div class="page_where_r"><a href="javascript:history.go(-1);" rel="nofollow">&laquo; Go Back</a></div>
     <div class="blank8px"></div>
@@ -8,7 +10,7 @@
         <div class="blank10px"></div>
 
         <form action="order.asp" method="post" name="formAddOrder">
-            <table class="tab_comm">
+            <table class="tab_comm" id="#shopping-cart-table">
                 <tr class="tr_head">
                     <td width="120">&nbsp;</td>
                     <td>Product Name</td>
@@ -16,20 +18,28 @@
                     <td width="100">Qty.</td>
                     <td width="100">Subtotal</td>
                 </tr>
-
+                <?php foreach($cart_info['products'] as $product_one): ?>
                 <tr class="tr_info">
-                    <td><div class="img100px"><a href="/wholesale/latest-mb-sd-c4-c5-software-hdd.html" title="Newest 500GB V2017.07 MB SD C4/C5 Software HDD For DELL D630 Support WIN7&amp;WIN10 System" target="_blank"><img src="/upload/pro/500gb-mb-sd-connect-compact-c4-software-hdd-180.jpg" width="100" height="100" border="0" hspace="0" vspace="0" alt="Newest 500GB V2017.07 MB SD C4/C5 Software HDD For DELL D630 Support WIN7&amp;WIN10 System" align="absmiddle" /></a></div></td>
+                    <td><div class="img100px"><a href="<?= $product_one['url'] ?>" title="<?= $product_one['name'] ?>" target="_blank">
+                                <img src="<?= Yii::$service->product->image->getResize($product_one['image'],[100,100],false) ?>" width="100" height="100" border="0" hspace="0" vspace="0" alt="<?= $product_one['name'] ?>" align="absmiddle" /></a>
+                        </div></td>
                     <td class="align_left gray">
-                        <a href="/wholesale/latest-mb-sd-c4-c5-software-hdd.html"><span class="px13">Newest 500GB V2017.07 MB SD C4/C5 Software HDD For DELL D630 Support WIN7&amp;WIN10 System</span></a>
+                        <a href="<?= $product_one['url'] ?>"><span class="px13"><?= $product_one['name'] ?></span></a>
                         <div class="blank10px"></div>
-                        Item No. SS195-D7
-                        <div class="blank10px"></div><a href="javascript:void(0)" onclick="javascript:OrderRemove(&quot;69820|-|-&quot;)">remove</a>
+                        Item No.<?= $product_one['sku'] ?>
+                        <div class="blank10px"></div>
+                        <a href="javascript:void(0)"  rel="<?= $product_one['item_id']; ?>" title="Remove item" class="btn-remove btn-remove2"><?= Yii::$service->page->translate->__('Remove item');?></a>
                     </td>
-                    <td>$109.00</td>
-                    <td><input name="Q_69820|-|-" id="Q_69820|-|-" type="text" class="input" onkeypress="event.returnValue=IsDigit();" onkeyup="ShowHideUpdateSpan(this,&quot;1&quot;,&quot;txtUpdate0&quot;,&quot;69820|-|-&quot;);" size="4" maxlength="6" value="1" style="width:50px;"><span class="px11" id="txtUpdate0"></span></td>
-                    <td><b>$109.00</b></td>
+                    <td><?=  $currency_info['symbol'];  ?><?= Format::price($product_one['product_price']); ?></td>
+                    <td>
+                        <a href="javascript:void(0)" class="cartqtydown changeitemqty" rel="<?= $product_one['item_id']; ?>" num="<?= $product_one['qty']; ?>"></a>
+                        <input name="cart[qty]" size="4" title="Qty" class="input-text qty input" rel="<?= $product_one['item_id']; ?>" maxlength="12" value="<?= $product_one['qty']; ?>">
+                        <a href="javascript:void(0)" class="cartqtyup changeitemqty" rel="<?= $product_one['item_id']; ?>" num="<?= $product_one['qty']; ?>"></a>
+                        <div class="clear"></div>
+                    </td>
+                    <td><b><?=  $currency_info['symbol'];  ?><?= Format::price($product_one['product_row_price']); ?></b></td>
                 </tr>
-
+                <?php  endforeach;  ?>
                 <tr class="tr_info" id="trShipToCountryChoose">
                     <td colspan="5" class="align_right px11">
 
@@ -43,50 +53,30 @@
 
                 <tr class="tr_info">
                     <td colspan="5" class="align_right verdana line18em">
-                        <b>Items Total: <span class="red_dark"> $109.00 </span></b>&nbsp;&nbsp;&nbsp;
-                        &nbsp;&nbsp;&nbsp;<b><span class="green">Free Shipping</span></b>
+                        <b>Items Total: <span class="red_dark"><?=  $currency_info['symbol'];  ?><?= Format::price($cart_info['product_total']); ?></span></b>&nbsp;&nbsp;&nbsp;
+                        <b><span class="green">Shipping Cost:<?=  $currency_info['symbol'];  ?><?= Format::price($cart_info['shipping_cost']); ?></span></b>
                         <br />
-                        <b class="red px16">Total Sum: $109.00</b>
+                        <b class="red px16">Total Sum:<?=  $currency_info['symbol'];  ?><?= Format::price($cart_info['grand_total']) ?></b>
                     </td>
                 </tr>
                 <tr class="tr_info">
                     <td colspan="5" class="align_right">
-                        <span id="v_apply_couponcode" style="display:"><strong>Coupon Code: </strong><input name="CouponCode" type="text" id="CouponCode" size="15" maxlength="15" style="width:80px; height:18px; margin-top:10px;" />
-                                        <input name="button" type="button" class="btn_near" id="button" value="Apply" onclick="javascript:OrderSetCouponCode();" />
-                        </span>
-                        <span id="v_mark_couponcode" class="px11 gray_dark" style="display:none;">
-                                <a href="javascript:void(0);" onclick="document.getElementById('v_apply_couponcode').style.display='';document.getElementById('v_mark_couponcode').style.display='none'; return false;">Click here to change.</a>
+                        <span id="v_apply_couponcode" style="display:"><strong>Coupon Code: </strong>
+							<input type="hidden" class="couponType"  value="<?= $cart_info['coupon_code'] ? 1 : 2 ; ?>"  />
+							<input type="text" style="width:80px; height:18px; margin-top:10px;"  id="coupon_code" name="coupon_code" value="<?= $cart_info['coupon_code']; ?>">
+                            <a href="javascript:void(0)" class="btn_near"><span><span><?= Yii::$service->page->translate->__($cart_info['coupon_code'] ? 'Cancel Coupon' : 'Apply') ; ?></span></span> </a>
                         </span>
                     </td>
                 </tr>
             </table>
-            <input type="hidden" name="Action" value="" />
-            <input type="hidden" name="operateItem" value="" />
-            <input type="hidden" name="thisReturnURL" value="http://www.uobdii.com/wholesale/latest-mb-sd-c4-c5-software-hdd.html" />
-            <input name="OrderAddItemsYN" type="hidden" value="" />
         </form>
         <div class="blank10px"></div>
-        <div class="fl" style="margin-top:10px;"><input name="Continue_Shopping" type="button" class="btn_near btn_mid" value="Continue Shopping" onclick="javascript:window.location.href='http://www.uobdii.com/wholesale/latest-mb-sd-c4-c5-software-hdd.html';return false;" /></div>
-        <div class="float_right" style="margin-top:10px;"> -- OR -- <input name="Proceed_to_Checkout" type="button" class="btn_near btn_mid" value="Proceed to Checkout" onclick="javascript:GotoWhereAction('Submit');return false;" /></div>
+        <div class="fl" style="margin-top:10px;"><input name="Continue_Shopping" type="button" class="btn_near btn_mid" value="Continue Shopping" onclick="location.href='<?= Yii::$service->url->getUrl('checkout/onepage');  ?>'" />paypal</div>
+        <div class="float_right" style="margin-top:10px;"> -- OR --
+            <a class="btn_near btn_mid" href="<?= Yii::$service->url->getUrl('payment/paypal/express/start');    ?>">Proceed to Checkout</a>
+        </div>
         <div class="float_right" style="margin-right:10px;">
-            <form action="/api_ppec/paypal_ec_redirect.php" method="POST" id="form_jumpto_checkout_papal_ecs">
-                <input type="hidden" name="paymentType" value="Sale">
-                <input type="hidden" name="currencyCodeType" value="USD" />
-                <input type="hidden" name="L_PAYMENTREQUEST_0_NAME0" value="Newest+500GB+V2017%2E07+MB+SD+C4%2FC5+Software+HDD+For+DELL+D630+Support+WIN7%26amp%3BWIN10+System" />
-                <input type="hidden" name="L_PAYMENTREQUEST_0_NUMBER0" value="SS195-D7" />
-                <input type="hidden" name="L_PAYMENTREQUEST_0_AMT0" value="109.00" />
-                <input type="hidden" name="L_PAYMENTREQUEST_0_QTY0" value="1" />
-                <input type="hidden" name="L_PAYMENTREQUEST_0_DESC0" value="69820; Free Shipping; %2D; %2D; 275g" />
-                <input type="hidden" name="PAYMENTREQUEST_0_ITEMAMT" value="109.00" />
-                <input type="hidden" name="PAYMENTREQUEST_0_SHIPPINGAMT" value="0.00">
-                <input type="hidden" name="PAYMENTREQUEST_0_AMT" value="109.00">
-                <input type="hidden" name="PAYMENTREQUEST_0_CUSTOM" value="0|351041|0.0||0.0|0.00|0|275|http://www.UOBDII.com" />
-                <input type="hidden" name="PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE" value="CN" />
-                <input type="hidden" name="myOrderProNum" value="0" />
-                <input type="hidden" name="LOGOIMG" value="http://www.UOBDII.com/images/logo.png">
-                <input name="" type="image" class="ipt_img" src="/images/pay/pp-checkout-logo-large.png" alt="Check out with PayPal" id="myPPECbutton" />
-                <div id="myContDiID"></div>
-            </form>
+
             <script type="text/javascript">
                 window.paypalCheckoutReady = function () {
                     paypal.checkout.setup('sinpecal_api1.gmail.com', {
@@ -110,3 +100,123 @@
         <div class="clear"></div>
     </div>
 </div>
+<script>
+    // add to cart js
+    <?php $this->beginBlock('changeCartInfo') ?>
+    $(document).ready(function(){
+        currentUrl = "<?= Yii::$service->url->getUrl('checkout/cart') ?>"
+        updateCartInfoUrl = "<?= Yii::$service->url->getUrl('checkout/cart/updateinfo') ?>"
+        $(".cartqtydown").click(function(){
+            $item_id = $(this).attr("rel");
+            num = $(this).attr("num");
+            if(num > 1){
+                $data = {
+                    item_id:$item_id,
+                    up_type:"less_one"
+                };
+                jQuery.ajax({
+                    async:true,
+                    timeout: 6000,
+                    dataType: 'json',
+                    type:'get',
+                    data: $data,
+                    url:updateCartInfoUrl,
+                    success:function(data, textStatus){
+                        if(data.status == 'success'){
+                            window.location.href=currentUrl;
+                        }
+                    },
+                    error:function (XMLHttpRequest, textStatus, errorThrown){}
+                });
+            }
+        });
+
+        $(".cartqtyup").click(function(){
+            $item_id = $(this).attr("rel");
+            $data = {
+                item_id:$item_id,
+                up_type:"add_one"
+            };
+            jQuery.ajax({
+                async:true,
+                timeout: 6000,
+                dataType: 'json',
+                type:'get',
+                data: $data,
+                url:updateCartInfoUrl,
+                success:function(data, textStatus){
+                    if(data.status == 'success'){
+                        window.location.href=currentUrl;
+                    }
+                },
+                error:function (XMLHttpRequest, textStatus, errorThrown){}
+            });
+
+        });
+
+        $(".btn-remove").click(function(){
+            $item_id = $(this).attr("rel");
+
+            $data = {
+                item_id:$item_id,
+                up_type:"remove"
+            };
+            jQuery.ajax({
+                async:true,
+                timeout: 6000,
+                dataType: 'json',
+                type:'get',
+                data: $data,
+                url:updateCartInfoUrl,
+                success:function(data, textStatus){
+                    if(data.status == 'success'){
+                        window.location.href=currentUrl;
+                    }
+                },
+                error:function (XMLHttpRequest, textStatus, errorThrown){}
+            });
+
+        });
+
+        $(".add_coupon_submit").click(function(){
+            coupon_code = $("#coupon_code").val();
+            coupon_type = $(".couponType").val();
+            coupon_url = "";
+            if(coupon_type == 2){
+                coupon_url = "<?=  Yii::$service->url->getUrl('checkout/cart/addcoupon'); ?>";
+            }else if(coupon_type == 1){
+                coupon_url = "<?=  Yii::$service->url->getUrl('checkout/cart/cancelcoupon'); ?>";
+            }
+            if(!coupon_code){
+                alert("coupon can not empty!");
+            }
+            //coupon_url = $("#discount-coupon-form").attr("action");
+            jQuery.ajax({
+                async:true,
+                timeout: 6000,
+                dataType: 'json',
+                type:'post',
+                data: {"coupon_code":coupon_code},
+                url:coupon_url,
+                success:function(data, textStatus){
+                    if(data.status == 'success'){
+                        window.location.href=currentUrl;
+                    }else if(data.content == 'nologin'){
+                        window.location.href="<?=  Yii::$service->url->getUrl('customer/account/login'); ?>";
+                    }else{
+                        $(".coupon_add_log").html(data.content);
+                    }
+                },
+                error:function (XMLHttpRequest, textStatus, errorThrown){}
+            });
+
+
+        });
+
+
+
+    });
+
+    <?php $this->endBlock(); ?>
+    <?php $this->registerJs($this->blocks['changeCartInfo'],\yii\web\View::POS_END);//将编写的js代码注册到页面底部 ?>
+</script>
