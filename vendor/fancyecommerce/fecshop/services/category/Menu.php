2d413868f3d530f9a0b86776b5092e86a078b10d
iwisunny <iwisunny@gmail.com>
2017-09-03 13:14:41 +0800

    修改分类

--- vendor/fancyecommerce/fecshop/services/category/Menu.php
+++ vendor/fancyecommerce/fecshop/services/category/Menu.php
@@ -22,12 +22,14 @@ class Menu extends Service
     public $rootCategoryId = '0';
     protected $_categoryModelName = '\fecshop\models\mongodb\Category';
     protected $_categoryModel;
-    
-    public function __construct(){
-        list($this->_categoryModelName,$this->_categoryModel) = Yii::mapGet($this->_categoryModelName);  
+
+    public function __construct()
+    {
+        list($this->_categoryModelName, $this->_categoryModel) = Yii::mapGet($this->_categoryModelName);
     }
+
     /**
-     * @property $parentId | int 
+     * @property $parentId | int
      * 得到分类的目录信息
      */
     protected function actionGetCategoryMenuArr($parentId = '')
@@ -41,18 +43,18 @@ class Menu extends Service
             '_id', 'parent_id', 'name', 'url_key', 'menu_custom',
         ])->where([
             'parent_id' => $parentId,
-            'status'    => $model::STATUS_ENABLE,
+            'status' => $model::STATUS_ENABLE,
             'menu_show' => $model::MENU_SHOW,
         ])->all();
         if (is_array($data) && !empty($data)) {
             foreach ($data as $category) {
                 $categoryOne = [
-                    '_id'           => (string) $category['_id'],
-                    'name'          => Yii::$service->store->getStoreAttrVal($category['name'], 'name'),
-                    'menu_custom'   => Yii::$service->store->getStoreAttrVal($category['menu_custom'], 'menu_custom'),
-                    'url'           => Yii::$service->url->getUrl($category['url_key']),
+                    '_id' => (string)$category['_id'],
+                    'name' => Yii::$service->store->getStoreAttrVal($category['name'], 'name'),
+                    'menu_custom' => Yii::$service->store->getStoreAttrVal($category['menu_custom'], 'menu_custom'),
+                    'url' => Yii::$service->url->getUrl($category['url_key']),
                 ];
-                $childMenu = $this->getCategoryMenuArr((string) $category['_id']);
+                $childMenu = $this->getCategoryMenuArr((string)$category['_id']);
                 if ($childMenu) {
                     $categoryOne['childMenu'] = $childMenu;
                 }
@@ -72,12 +74,40 @@ class Menu extends Service
     protected function hasChild($categoryId)
     {
         $one = $this->_categoryModel->find()->asArray()->where([
-                'parent_id' => $categoryId,
-            ])->one();
+            'parent_id' => $categoryId,
+        ])->one();
         if ($one['_id']) {
             return true;
         }
 
         return false;
     }
+
+    protected function actionGetChildCate($parentId = '')
+    {
+        $model = $this->_categoryModel;
+        $arr = [];
+        if (!$parentId) {
+            $parentId = $this->rootCategoryId;
+        }
+        $data = $this->_categoryModel->find()->asArray()->select([
+            '_id', 'parent_id', 'name', 'url_key', 'menu_custom',
+        ])->where([
+            'parent_id' => $parentId,
+            'status' => $model::STATUS_ENABLE,
+        ])->all();
+        if (is_array($data) && !empty($data)) {
+            foreach ($data as &$category) {
+                $category = [
+                    '_id' => (string)$category['_id'],
+                    'name' => Yii::$service->store->getStoreAttrVal($category['name'], 'name'),
+                    'menu_custom' => Yii::$service->store->getStoreAttrVal($category['menu_custom'], 'menu_custom'),
+                    'url' => Yii::$service->url->getUrl($category['url_key']),
+                ];
+            }
+            return $data;
+        }
+        return '';
+    }
+
 }
