e0599c16aead4b8dfe5ecdf04fbb8a52a000f53b
iwisunny <iwisunny@gmail.com>
2017-09-13 13:25:48 +0800

    修改

--- appfront/theme/terry/theme01/customer/leftmenu.php
+++ appfront/theme/terry/theme01/customer/leftmenu.php
@@ -28,8 +28,9 @@
 
     <div class="fun_column px11 gray word_wrap word_break">
         <div class="clear"></div>
+
         <!-- 账户信息-->
-        <b class="px14">Welcome!</b><br/> <b class="px12"><?php echo Yii::$app->user->identity->firstname;?>&nbsp;&nbsp;<?php echo Yii::$app->user->identity->lastname;?></b><br/>User ID: <span class="email"><?php echo Yii::$app->user->identity->email;?></span><br/>E-Mail: <span class="email"><?php echo Yii::$app->user->identity->email;?></span><br/>Account Status:<b>Normal</b><br/>
+        <b class="px14">Welcome!</b><br/> <b class="px12"><?php echo Yii::$app->user->identity->firstname;?>&nbsp;&nbsp;<?php echo Yii::$app->user->identity->lastname;?></b><br/>User ID: <span class="email"><?php echo Yii::$app->user->identity->email;?></span><br/>E-Mail: <span class=""><?php echo Yii::$app->user->identity->email;?></span><br/>Account Status:<b>Normal</b><br/>
         <!-- 账户信息-->
         <div class="dashed5px"></div>
         <div class="align_right">
