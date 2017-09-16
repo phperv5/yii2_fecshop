18fe4c5843805dbcd54c7173d5eddcf03a248939
iwisunny <iwisunny@gmail.com>
2017-09-16 17:31:16 +0800

    no message

--- appfront/theme/terry/theme01/widgets/head.php
+++ appfront/theme/terry/theme01/widgets/head.php
@@ -15,6 +15,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <?php $parentThis->head() ?>
 <!-- livezilla.net PLACE SOMEWHERE IN BODY -->
-<script type="text/javascript" id="9ee63f4528e2de7c8be9cb46235930ec" src="http://115.28.80.138:200/script.php?id=9ee63f4528e2de7c8be9cb46235930ec"></script>
+<script type="text/javascript" id="33142d219d33f5ca01861e80a90aebcc" src="http://115.28.80.138:200/script.php?id=33142d219d33f5ca01861e80a90aebcc"></script>
 <!-- livezilla.net PLACE SOMEWHERE IN BODY -->
 <!--<script type="text/javascript" id="85db553d33449400ad49117cfd638a08" src="http://ls.uobdii.org/script.php?id=85db553d33449400ad49117cfd638a08"></script>-->
