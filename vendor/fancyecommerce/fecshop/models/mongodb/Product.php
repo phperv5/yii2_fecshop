70977c30dcf59f231ea37948e576b03aa40b9ce2
iwisunny <iwisunny@gmail.com>
2017-09-16 12:11:08 +0800

    添加package

--- vendor/fancyecommerce/fecshop/models/mongodb/Product.php
+++ vendor/fancyecommerce/fecshop/models/mongodb/Product.php
@@ -49,6 +49,7 @@ class Product extends ActiveRecord
             'spu',
             'sku',
             'weight',
+            'package',
             'score',
             'status',
             'qty',
