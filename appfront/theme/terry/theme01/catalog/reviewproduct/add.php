1c9f88726d47f7baae9537b95e18aa22377cf536
iwisunny <iwisunny@gmail.com>
2017-09-16 09:23:32 +0800

    修改

--- appfront/theme/terry/theme01/catalog/reviewproduct/add.php
+++ appfront/theme/terry/theme01/catalog/reviewproduct/add.php
@@ -1,146 +1,128 @@
-<?php
-/**
- * FecShop file.
- *
- * @link http://www.fecshop.com/
- * @copyright Copyright (c) 2016 FecShop Software LLC
- * @license http://www.fecshop.com/license/
- */
-?>
-<div class="main container one-column">
-<?= Yii::$service->page->widget->render('flashmessage'); ?>
-	<div class="col-main">
-		<div class="std">
-			<div class="product-Reviews_top">
-				<div style="width:90px;float:left;">
-					<a href="<?= $url  ?>">
-						<img src="<?= Yii::$service->product->image->getResize($main_img,[80,80],false) ?>">
-					</a>
-				</div>
-				
-				<div style="width:700px;float:left;">
-					<div style="">
-						<a href="<?= $url  ?>"><?= $product_name ?></a> 
-					</div>
-					<div class="product_info review_add_price">
-						<div class="price_info">
-							<?php # 价格部分
-								$priceView = [
-									'view'	=> 'catalog/product/index/price.php'
-								];
-								$priceParam = [
-									'price_info' => $price_info,
-								];
-							?>
-							<?= Yii::$service->page->widget->render($priceView,$priceParam); ?>
-						</div>
-					</div>
-				</div>
-				<div class="clear"></div>
-			</div>
-			
-			<div class="product-Reviews_bottom">
-				<form method="post" action="">
-					<?= \fec\helpers\CRequest::getCsrfInputHtml();  ?>
-					<input name="editForm[product_spu]" value="<?= $spu ?>" id="product_spu" type="hidden">
-					<input name="editForm[product_id]" value="<?= $product_id ?>" id="product_id" type="hidden">
-					
-					<div class="h-30">
-						<div class="lh30_f">
-							<strong><?= Yii::$service->page->translate->__('Rate');?></strong><em class="product-description_em">*</em>&nbsp;&nbsp;&nbsp;&nbsp;
-						</div>
-						<div class="lh30_f">
-							<input name="editForm[rate_star]" value="5" id="review_price_field" type="hidden">
-							<ul class="star_ul">
-								<li><a class="star_1 full_star" title="1 stars" rel="1" alt="6" href="#" onclick="javascript:return false;"></a></li>
-								<li><a class="star_2 full_star" title="2 stars" rel="2" alt="7" href="#" onclick="javascript:return false;"></a></li>
-								<li><a class="star_3 full_star" title="3 stars" rel="3" alt="8" href="#" onclick="javascript:return false;"></a></li>
-								<li><a class="star_4 full_star" title="4 stars" rel="4" alt="9" href="#" onclick="javascript:return false;"></a></li>
-								<li><a class="star_5 full_star" title="5 stars" rel="5" alt="10" href="#" onclick="javascript:return false;"></a></li>
-							</ul>          
-							<span class="review_span_error" id="review_price_span" style="display:inline;"></span>
-						</div>
-					</div>
-					
-				
-					<div id="review_create">
-						<ul id="reviews_form_list">
-							<li>
-								<label for="nickname_field" class="required">
-								<strong><?= Yii::$service->page->translate->__('Your Name');?></strong><em class="product-description_em">*</em></label>
-								<div class="input-box">
-									
-									<input name="editForm[name]" id="review_email_field" class="input-text  review-input-text required-entry" value="<?=  $editForm['name'] ? $editForm['name'] : $customer_name ?>" type="text">
-								</div>
-								<span class="review_span_error" id="review_email_span"></span>
-							</li>
-							<li>
-								<label for="nickname_field" class="required">
-								<strong><?= Yii::$service->page->translate->__('Summary of Your Review');?></strong><em class="product-description_em">*</em></label>
-								<div class="input-box">
-									<input name="editForm[summary]" id="review_title_field" class="input-text  review-input-text required-entry" value="<?=  $editForm['summary'] ?>" type="text">
-								</div>
-								<span class="review_span_error" id="review_title_span"></span>
-							</li>
-							
-							
-							<li id="review_textarea" style="width: 722px;">
-								<label for="nickname_field" class="required"><strong><?= Yii::$service->page->translate->__('Review');?></strong><em class="product-description_em">*</em></label>
-								<div class="input-box">
-									<textarea name="editForm[review_content]" id="review_review_field"><?=  $editForm['review_content'] ?></textarea>
-								</div>
-								<span class="review_span_error" id="review_review_span"></span>
-							</li>
-							
-							<?php if($add_captcha):  ?>
-							<li style="width:700px;">
-								<label for="captcha" class="required"><em>*</em><?= Yii::$service->page->translate->__('Captcha');?></label>
-								<div class="input-box login-captcha">
-									<input type="text" name="editForm[captcha]" value="" size=10 class="login-captcha-input"> 
-									<img class="login-captcha-img"  title="点击刷新" src="<?= Yii::$service->url->getUrl('site/helper/captcha'); ?>" align="absbottom" onclick="this.src='<?= Yii::$service->url->getUrl('site/helper/captcha'); ?>?'+Math.random();"></img>
-									<i class="refresh-icon"></i>
-								</div>
-								<script>
-								<?php $this->beginBlock('login_captcha_onclick_refulsh') ?>  
-								$(document).ready(function(){
-									$(".refresh-icon").click(function(){
-										$(this).parent().find("img").click();
-									});
-								});
-								<?php $this->endBlock(); ?>  
-								</script>  
-								<?php $this->registerJs($this->blocks['login_captcha_onclick_refulsh'],\yii\web\View::POS_END);//将编写的js代码注册到页面底部 ?>
+<div class="main">
+    <div class="page_where_l"><a href="/">Home</a> - <?= $product_name ?>
+    </div>
+    <div class="page_where_r"><a href="javascript:history.go(-1);" rel="nofollow">&laquo; Go Back</a></div>
+    <div class="blank8px"></div>
+    <div class="exh_full_top"></div>
+    <div class="exh_full_main">
+        <div class="blank15px"></div>
+        <fieldset>
+            <div class="pro_list pro_list_nd">
+                <div class="photo">
+                    <a href="<?= $url  ?>" title="<?= $product_name ?>">
+                        <img src="<?= Yii::$service->product->image->getResize($main_img,[120,120],false) ?>" width="120" height="120" border="0" hspace="0" vspace="0" alt="<?= $product_name ?>" align="absmiddle"/></a></div>
+                <div class="brief">
 
-							</li>
-							<?php endif;  ?>
-							<li>
-								<button type="submit" title="Submit Review" class="button" id="m_top_10" onclick="return check_review()"><span><span><?= Yii::$service->page->translate->__('Submit');?></span></span></button>
-							</li>
-						</ul>
-						<div class="clear"></div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+                    <h2><span class="specialoffer"></span><a href="<?= $url  ?>"><?= $product_name ?></a></h2>
+                    <?= $product_name ?>
+                    <div class="blank10px"></div>
+<!--                    <span class="px11">Item No.--><?//= $sku ?><!--</span>-->
+                    <div class="clear"></div>
+                </div>
+                <div class="order_fun px11">
+                    <div class="blank10px"></div>
+
+                    <span class="pro_pri_tit_vip_m">Buy It Now:</span>
+                    <span class="pro_pri_curr_vip_m" name="cc_v_USD" style="display:">
+                        <?= $price_info['price']['symbol'] ?><?= $price_info['price']['value'] ?>
+                    </span>
+                    <div class="clear"></div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
+
+        </fieldset>
+
+        <a name="WriteReview"></a>
+        <div class="blank15px"></div>
+        <div class="blank15px"></div>
+        <div class="blank15px"></div>
+
+        <a name="na_write_rev"></a>
+        <div class="fc_col_b">
+            <span class="fc_tit">Write a Review</span>
+            <form action="" method="post" name="form_rev_add">
+                <?= \fec\helpers\CRequest::getCsrfInputHtml();  ?>
+                <dl class="px900">
+                    <dt><span class="red">*</span> Rating:</dt>
+                    <dd>
+                        <div class="rate_star">
+                            <span class="star_casing star_1" id="star_casing_1" rel="1"></span>
+                            <span class="star_casing star_2" id="star_casing_2"" rel="2"></span>
+                            <span class="star_casing star_3" id="star_casing_3"" rel="3"></span>
+                            <span class="star_casing star_4" id="star_casing_4"" rel="4"></span>
+                            <span class="star_casing star_5" id="star_casing_5"  rel="5"></span>
+                        </div>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<span class="px11" id="txt_pro_RateStars">Please select rating stars.</span>
+                        <input name="editForm[rate_star]" value="5" id="review_price_field" type="hidden">
+                        <div class="clear"></div>
+                    </dd>
+                    <div class="clear"></div>
+
+
+                    <dt><span class="red">*</span> Your First &amp; Last Name:</dt>
+                    <dd>
+                        <input name="editForm[name]" type="text" id="ReviewerName" size="30" maxlength="50"
+                               class="input_normal" onfocus="InputCss(this,'focus');"
+                               onblur="CheckVerifyReviewerName(this,'txt_ReviewerName');">
+                        <span id="txt_ReviewerName" class="alert"></span>
+                    </dd>
+
+                    <dt><span class="red">*</span> Your E-Mail Address:</dt>
+                    <dd><input name="editForm[summary]" type="text" id="ReviewerEmail" size="45" maxlength="50"
+                               class="input_normal" onfocus="InputCss(this,'focus');"
+                               onblur="CheckVerifyReviewerEmail(this,'txt_ReviewerEmail');">
+                        <span id="txt_ReviewerEmail" class="alert"></span>
+                    </dd>
+
+
+                    <dt><span class="red">*</span> Review Detail:</dt>
+                    <dd>
+                        <textarea name="editForm[review_content]" rows="6" id="ReviewDetail" class="input_normal"
+                                  onfocus="InputCss(this,'focus');"
+                                  onblur="CheckVerifyReviewDetail(this,'txt_ReviewDetail');"
+                                  style="width:90%;"></textarea><span id="txt_ReviewDetail" class="alert"></span>
+                    </dd>
+
+                    <dt><span class="red_star">*</span>Verification Code:</dt>
+                    <dd><input name="numVerify" id="numVerify" type="text" maxlength="4" size="10"
+                               onkeypress="event.returnValue=IsDigit();" class="input_normal"
+                               onfocus="InputCss(this,'focus');" onblur="CheckNumVerify(this,'txtIDnumVerify');">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span
+                                class="span_num_verify" id="span_id_num_verify">6974</span><input
+                                name="numVerifyConfirm" id="numVerifyConfirm" type="hidden" value="6974"/><span
+                                id="txtIDnumVerify" class="alert"></span>&nbsp;&nbsp;&nbsp;&nbsp;<b class="px11"><a
+                                    href="javascript:void(0);" onclick="GetNewVerifyCode();">No right? Get a new
+                                Verification Code</a></b></dd>
+
+                    <dt>&nbsp;</dt>
+                    <dd><input type="submit" name="button" id="button" value="Submit Review" class="btn_submit"></dd>
+
+                </dl>
+
+            </form>
+            <div class="blank5px"></div>
+            <div class="dashed5px"></div>
+    </div>
+    <div class="exh_full_bottom"></div>
+    <div class="clear"></div>
 </div>
-
-<script>
-	// add to cart js	
-	<?php $this->beginBlock('product_review_rate') ?>
-	$(document).ready(function(){
-	   $(".star_ul li a").click(function(){
-                $(".star_ul li a").removeClass('full_star');
-                $(this).addClass('full_star');
+    <script>
+        // add to cart js
+        <?php $this->beginBlock('product_review_rate') ?>
+        $(document).ready(function(){
+            $(".rate_star .star_casing").click(function(){
+                $(".rate_star .star_casing").removeClass('star_casing_on');
+                $(this).addClass('star_casing_on');
                 $num = $(this).attr('rel');
                 for($i=1;$i<=$num;$i++){
-                   $('.star_'+$i).addClass('full_star');
+                    $('.star_'+$i).addClass('star_casing_on');
                 }
                 $('#review_price_field').val($num);
+            });
         });
-	});
-	 
-	<?php $this->endBlock(); ?>  
-	<?php $this->registerJs($this->blocks['product_review_rate'],\yii\web\View::POS_END);//将编写的js代码注册到页面底部 ?>
 
-</script> 
+        <?php $this->endBlock(); ?>
+        <?php $this->registerJs($this->blocks['product_review_rate'],\yii\web\View::POS_END);//将编写的js代码注册到页面底部 ?>
+
+    </script>
