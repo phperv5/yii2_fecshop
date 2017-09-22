ea6b0900daecf121789517e4b60dfb3da72a5fd1
USER-20150613FK\Administrator <jerry@2bai.com.cn>
2017-09-22 23:43:57 +0800

    test

--- appfront/theme/terry/theme01/catalog/product/index/price.php
+++ appfront/theme/terry/theme01/catalog/product/index/price.php
@@ -10,8 +10,8 @@
 <?php  $price_info = $parentThis['price_info'];   ?>
 <?php if(isset($price_info['special_price']['value'])):  ?>
     <span class="pro_pri_tit_sale">Latest price:</span>
-    <span class="pro_pri_curr_speical" name="cc_v_USD" style="display:"><strong><?= $price_info['price']['symbol']  ?><?= $price_info['special_price']['value'] ?></strong></span>
-    <span class="pro_pri_tit_vip">Buy It Now:</span><span class="pro_pri_curr_vip" name="cc_v_USD" style="display:"><?= $price_info['price']['symbol']  ?><?= $price_info['price']['value'] ?></span>
+    <span class="pro_pri_curr_speical" name="cc_v_USD" style="display:"><strong><?= $price_info['price']['symbol']  ?><?= $price_info['price']['value'] ?></strong></span>
+    <span class="pro_pri_tit_vip">Buy It Now:</span><span class="pro_pri_curr_vip" name="cc_v_USD" style="display:"><?= $price_info['special_price']['symbol']  ?><?= $price_info['special_price']['value'] ?></span>
 <?php else:  ?>
 	<div class="price no-special">
         <span class="pro_pri_tit_vip">Buy It Now:</span><span class="pro_pri_curr_vip" name="cc_v_USD" style="display:"><?= $price_info['price']['symbol']  ?><?= $price_info['price']['value'] ?></span>
