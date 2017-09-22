fc64b8a623fd4afe30d9b358704e5121553cb018
iwisunny <iwisunny@gmail.com>
2017-09-22 13:49:35 +0800

    修改

--- appfront/theme/terry/theme01/catalog/product/index/price.php
+++ appfront/theme/terry/theme01/catalog/product/index/price.php
@@ -9,6 +9,8 @@
 ?>
 <?php  $price_info = $parentThis['price_info'];   ?>
 <?php if(isset($price_info['special_price']['value'])):  ?>
+    <span class="pro_pri_tit_sale">Latest price:</span>
+    <span class="pro_pri_curr_speical" name="cc_v_USD" style="display:"><strong><?= $price_info['price']['symbol']  ?><?= $price_info['special_price']['value'] ?></strong></span>
     <span class="pro_pri_tit_vip">Buy It Now:</span><span class="pro_pri_curr_vip" name="cc_v_USD" style="display:"><?= $price_info['price']['symbol']  ?><?= $price_info['price']['value'] ?></span>
 <?php else:  ?>
 	<div class="price no-special">
