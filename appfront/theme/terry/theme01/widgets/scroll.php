414333ccc631ecae976c9167913af81324ecc2ab
iwisunny <iwisunny@gmail.com>
2017-09-24 19:11:54 +0800

    修改

--- appfront/theme/terry/theme01/widgets/scroll.php
+++ appfront/theme/terry/theme01/widgets/scroll.php
@@ -1,2 +1,1 @@
-<!-- livezilla.net PLACE SOMEWHERE IN BODY -->
-<script type="text/javascript" id="fb9f264cb04c92774ee89ea898798494" src="http://115.28.80.138:200/script.php?id=fb9f264cb04c92774ee89ea898798494"></script>
+
