a82529a8184c98ddd697996c1719640a7e976e79
iwisunny <iwisunny@gmail.com>
2017-09-27 21:06:38 +0800

    修改

--- appfront/theme/terry/theme01/widgets/header.php
+++ appfront/theme/terry/theme01/widgets/header.php
@@ -29,7 +29,7 @@
                 <div class="hd_wr_nav"><a href="support/support.html">Help</a>
                     <ul>
                         <a href="<?= Yii::$service->url->getUrl('how-we-ship-the-item-to-you') ?>">Help Center</a>
-                        <a href="support/msg_add.asp.html">Freed Back</a>
+                        <a href="<?= Yii::$service->url->getUrl('freed back') ?>">Freed Back</a>
                     </ul>
                 </div>
             </div>
