aeb053578679b21d8eb7eb1ef245d00d94e4afb6
iwisunny <iwisunny@gmail.com>
2017-09-27 21:04:48 +0800

    修改配置

--- appfront/theme/terry/theme01/cms/article/index.php
+++ appfront/theme/terry/theme01/cms/article/index.php
@@ -10,59 +10,54 @@
         <div class="support_m_l"><b class="px13 verdana">UOBD2 Shipment Policy</b>
             <div class="support_m_l_list_area">
                 <a href="<?=  Yii::$service->url->getUrl('how-we-ship-the-item-to-you') ?>"">How we ship the item to you?</a>
-                <a href="/support/where-we-ship-to-3225.html">Where we ship to?</a>
-                <a href="/support/how-fast-you-can-get-your-product-1130.html">How fast you can get your product?</a>
-                <a href="/support/how-about-your-delivery-cost-4166.html">How about your delivery cost?</a>
-                <a href="/support/how-about-combine-shipping-4168.html">How about combine shipping?</a>
-                <a href="/support/how-about-change-your-shipping-method-and-shipping-address-4171.html">How about change your shipping method and shipping address?</a>
-                <a href="/support/how-about-track-your-package-4169.html">How about track your package?</a>
-                <a href="/support/what-could-cause-an-invalid-tracking-code-4172.html">What could cause an invalid tracking code?</a>
-                <a href="/support/what-should-you-do-when-you-receive-the-item-4170.html">What should you do when you receive the item?</a>
+                <a href="<?=  Yii::$service->url->getUrl('where-we-ship-to') ?>">Where we ship to?</a>
+                <a href="<?=  Yii::$service->url->getUrl('how-fast-you-can-get-your-product') ?>">How fast you can get your product?</a>
+                <a href="<?=  Yii::$service->url->getUrl('how-about-your-delivery-cost') ?>">How about your delivery cost?</a>
+                <a href="<?=  Yii::$service->url->getUrl('how-about-combine-shipping') ?>">How about combine shipping?</a>
+                <a href="<?=  Yii::$service->url->getUrl('how-about-change-your-shipping-method-and-shipping-address') ?>">How about change your shipping method and shipping address?</a>
+                <a href="<?=  Yii::$service->url->getUrl('how-about-track-your-package') ?>">How about track your package?</a>
+                <a href="<?=  Yii::$service->url->getUrl('what-could-cause-an-invalid-tracking-code') ?>">What could cause an invalid tracking code?</a>
+                <a href="<?=  Yii::$service->url->getUrl('what-should-you-do-when-you-receive-the-item') ?>">What should you do when you receive the item?</a>
             </div>
             <div class="blank5px"></div>
             <b class="px13 verdana">UOBD2 Return Policy</b>
             <div class="support_m_l_list_area">
-                <a href="/support/return-policy-4174.html">Return Policy</a>
-                <a href="/support/products-warranty-4198.html">Products Warranty</a>
-                <a href="/support/refund-policy-4177.html">Refund Policy</a>
-                <a href="/support/return-for-repair-4181.html">Return for repair</a>
+                <a href="<?=  Yii::$service->url->getUrl('return-policy') ?>">Return Policy</a>
+                <a href="<?=  Yii::$service->url->getUrl('products-warranty') ?>">Products Warranty</a>
+                <a href="<?=  Yii::$service->url->getUrl('refund-policy') ?>">Refund Policy</a>
+                <a href="<?=  Yii::$service->url->getUrl('return-for-repair') ?>">Return for repair</a>
             </div>
             <div class="blank5px"></div>
             <b class="px13 verdana">UOBD2 VIP Discount Card</b>
             <div class="support_m_l_list_area">
-                <a href="/support/vip-card-4182.html">VIP Card</a>
+                <a href="<?=  Yii::$service->url->getUrl('vip-card') ?>">VIP Card</a>
             </div>
             <div class="blank5px"></div>
             <b class="px13 verdana">UOBD2 Coupon Code</b>
             <div class="support_m_l_list_area">
-                <a href="/support/how-to-get-coupon-code-4183.html">How to get coupon code</a>
-                <a href="/support/how-to-use-coupon-code-4184.html">How to Use Coupon Code</a>
+                <a href="<?=  Yii::$service->url->getUrl('how-to-get-coupon-code') ?>">How to get coupon code</a>
+                <a href="<?=  Yii::$service->url->getUrl('how-to-use-coupon-code') ?>">How to Use Coupon Code</a>
             </div>
             <div class="blank5px"></div>
             <b class="px13 verdana">About my account</b>
             <div class="support_m_l_list_area">
-                <a href="/support/forget-account-password-4200.html">Forget Account Password?</a>
+                <a href="<?=  Yii::$service->url->getUrl('where-we-ship-to') ?>">Forget Account Password?</a>
             </div>
             <div class="blank5px"></div>
             <b class="px13 verdana">Tips to Use UOBD2</b>
             <div class="support_m_l_list_area">
-                <a href="/support/tips-to-use-uobdiicom-4217.html">Tips to Use UOBDII.com</a>
+                <a href="<?=  Yii::$service->url->getUrl('where-we-ship-to') ?>">Tips to Use UOBDII.com</a>
             </div>
             <div class="blank5px"></div>
             <b class="px13 verdana">Customs Clearance</b>
             <div class="support_m_l_list_area">
-                <a href="/support/customs-clearance-import-duty-4713.html">Customs Clearance, Import Duty</a>
+                <a href="<?=  Yii::$service->url->getUrl('where-we-ship-to') ?>">Customs Clearance, Import Duty</a>
             </div>
             <div class="blank5px"></div>
             <b class="px13 verdana">Newbie Guide</b>
             <div class="support_m_l_list_area">
-                <a href="/support/frequently-asked-questions-about-purchase-and-check-out-5007.html">Frequently Asked Questions About Purchase and Check Out</a>
-                <a href="/support/purchase-processing-in-uobd2net-5006.html">Purchase processing in UOBDII.com</a>
-            </div>
-            <div class="blank5px"></div>
-            <b class="px13 verdana">About UOBD2 Mobile APP Store</b>
-            <div class="support_m_l_list_area">
-                <a href="/support/download-android-apps-and-apple-apps-to-visit-uobdiicom-9004.html">Download Android Apps and Apple Apps to visit UOBDII.com</a>
+                <a href="<?=  Yii::$service->url->getUrl('frequently-asked-questions-about-purchase-and-check-out') ?>">Frequently Asked Questions About Purchase and Check Out</a>
+                <a href="<?=  Yii::$service->url->getUrl('purchase-processing') ?>">Purchase processing in UOBDII.com</a>
             </div>
             <div class="blank5px"></div>
             <br /><br /><br /><a href="msg_add.asp"><img src="/images/btn/submit_a_ticket.gif" alt="Submit a Ticket" border="0" /></a></div>
