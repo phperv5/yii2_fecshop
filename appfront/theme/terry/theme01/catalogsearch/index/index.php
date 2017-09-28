b3d634d1183e32aa57d75b5c2d9733c648c1a367
iwisunny <iwisunny@gmail.com>
2017-09-28 17:14:21 +0800

    修改搜索

--- appfront/theme/terry/theme01/catalogsearch/index/index.php
+++ appfront/theme/terry/theme01/catalogsearch/index/index.php
@@ -88,17 +88,23 @@
 
                             <div class="clear"></div>
                         </div>
-                        <?php
-                        $config = [
-                            'class' => 'fecshop\app\appfront\modules\Catalog\block\category\Price',
-                            'view' => 'catalog/category/price.php',
-                            'price' => $product['price'],
-                            'special_price' => $product['special_price'],
-                            'special_from' => $product['special_from'],
-                            'special_to' => $product['special_to'],
-                        ];
-                        echo Yii::$service->page->widget->renderContent('category_product_price', $config);
-                        ?>
+                        <div class="order_fun px11">
+                            <div class="blank5px"></div>
+                            <?php if(isset($product['special_price']) && !empty($product['special_price'])):  ?>
+                                <span class="pro_pri_tit_sale_s">Latest price:</span><span class="pro_pri_curr_sale_s" name="" style="display:"><strong><?=  $product['price']['symbol']. $product['price']['value'] ?></strong></span>
+                                <span class="pro_pri_tit_vip_m">Buy It Now:</span>
+                                <span class="pro_pri_curr_vip_m" name="" style="display:"><?= $product['special_price']['symbol'].$product['special_price']['value'] ?></span>
+                                <!--    <span class="pro_pri_of_sr" name="" style="display:">20% off</span>-->
+                            <?php else: ?>
+                                <span class="pro_pri_tit_vip_m">Buy It Now:</span>
+                                <span class="pro_pri_curr_vip_m" name="" style="display:"><?= $product['price']['symbol'].$product['price']['value'] ?></span>
+                            <?php endif;  ?>
+                            <div class="blank5px"></div>
+                            <div class="dashed_line"></div>
+                            <div class="blank5px"></div>
+                            <div class="blank5px"></div>
+                            <input name="add_to_cart" type="button" class="btn_addtocart_s" value="" title="Add to Cart" onclick="javascript:addProductToCart('<?= $product['_id'] ?>');return false;"/>
+                        </div>
                         <div class="clear"></div>
                     </div>
                 <?php endforeach; ?>
