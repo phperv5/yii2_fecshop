867223d3f62191831c32f254d7ee756328c270de
iwisunny <iwisunny@gmail.com>
2017-10-15 16:33:06 +0800

    commit

--- vendor/fancyecommerce/fecshop/app/appfront/modules/Customer/block/address/edit.php
+++ vendor/fancyecommerce/fecshop/app/appfront/modules/Customer/block/address/edit.php
@@ -1,168 +1,230 @@
-<div class="main">
-    <?php
-    $leftMenu = [
-        'class' => 'fecshop\app\appfront\modules\Customer\block\LeftMenu',
-        'view' => 'customer/leftmenu.php'
-    ];
-    ?>
-    <?= Yii::$service->page->widget->render($leftMenu, $this); ?>
-    <div class="main_scene">
-        <div class="exh_top"></div>
-        <div class="exh_main">
-            <div class="align_right px11 verdana" style="margin-top:-10px;"><a href="<?= $homeUrl ?>">Home</a> - <a href="<?= Yii::$service->url->getUrl('customer/order') ?>">My Account: <b class="red account-email"></b></a> - Edit Address Information</div><div class="blank5px"></div><h1>Edit Address Information</h1>
-            <form class="addressedit" action="<?= Yii::$service->url->getUrl('customer/address/edit'); ?>" method="post" id="form-validate">
-                <input name="address[address_id]" value="<?= $address_id; ?>" type="hidden">
-                <input name="redirect_url" value="<?= Yii::$app->request->get('redirect_url'); ?>" type="hidden">
-                <dl>
-                    <dt><span class="red_star">*</span>Received Name is:</dt>
-                    <dd>
-                        <div class="fl w200px"><b class="px11">First Name</b></div>
-                        <div class="fl w10px"></div>
-                        <div class="fl w200px"><b class="px11">Last Name</b></div>
-                        <div class="clear"></div>
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
 
-                        <div class="fl w200px">
-                            <input class="input-text required-entry input_normal" maxlength="255" title="First Name" value="<?= $first_name ?>" name="address[first_name]" id="firstname" type="text">
-                        </div>
+namespace fecshop\app\appfront\modules\Customer\block\address;
 
-                        <div class="fl w10px"></div>
-                        <div class="fl w200px">
-                            <input class="input-text required-entry input_normal" maxlength="255" title="Last Name" value="<?= $last_name ?>" name="address[last_name]" id="lastname" type="text">
-                        </div>
-                    </dd>
+use Yii;
 
-                    <dt><span class="red_star">*</span>Address Line1:</dt>
-                    <dd>
-                        <input class="input-text required-entry input_normal" maxlength="255" title="Last Name" value="<?= $street1 ?>" name="address[street1]" id="lastname" type="text">
-                    </dd>
+/**
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+class Edit
+{
+    public $_address_id;
+    public $_country;
+    public $_state;
+    public $_address;
 
-                    <dt>Address Line2:</dt>
-                    <dd>
-                        <input class="input-text optional input_normal" maxlength="255" title="street2" value="<?= $street2 ?>" name="address[street2]" id="lastname" type="text">
-                    </dd>
-
-                    <dt><span class="red_star">*</span>City:</dt>
-                    <dd>
-                        <input class="input-text required-entry input_normal" maxlength="255" title="Last Name" value="<?= $city ?>" name="address[city]" id="lastname" type="text">
-                    </dd>
-
-                    <dt><span class="red_star">*</span>Country / Region:</dt>
-                    <dd>
-                        <select id="address:country" class="address_country validate-select" title="Country" name="address[country]">
-                            <?= $countrySelect;  ?>
-                        </select>
-                    </dd>
-
-                    <dt><span class="red_star" id="is_must_or_not_addProvince" style="display:none">*</span>State/Province/Region:
-                    </dt>
-                    <dd>
-                        <div class="input-box state_html">
-                            <?= $stateHtml;  ?>
-                        </div>
-                    </dd>
-
-                    <dt>
-                        <span class="red_star">*</span>ZIP/Postal Code:
-                    </dt>
-                    <dd>
-                        <input class="input-text required-entry input_normal" maxlength="255" title="Last Name" value="<?= $zip ?>" name="address[zip]" id="lastname" type="text">
-                    </dd>
-
-                    <dt><span class="red_star">*</span>Phone Number:</dt>
-                    <dd>
-                        <input class="input-text required-entry input_normal" maxlength="255" title="Last Name" value="<?= $telephone ?>" name="address[telephone]" id="lastname" type="text">
-                    </dd>
-
-                    <!--                    <dt>Fax Number:</dt>-->
-                    <!--                    <dd><input name="addFax" id="addFax" type="text" size="35" maxlength="100" value="" class="input_normal" onfocus="InputCss(this,'focus');" onblur="InputCss(this,'');"></dd>-->
-
-                    <dt>&nbsp;</dt>
-                    <dd>
-                        <input name="address[is_default]" value="1" title="Save in address book" id="address:is_default" class="address_is_default checkbox" <?= $is_default_str; ?> type="checkbox"> Set it to my
-                        Default Address
-                    </dd>
-
-                    <div class="blank10px"></div>
-                    <dt>&nbsp;</dt>
-                    <dd><div  style="width: 112px;" class="btn_submit submitbutton" onclick="submit_address()">  Submit &amp; Save   </div></dd>
-                </dl>
-
-            </form>
-            <div class="clear"></div>
-        </div>
-        <div class="exh_bottom"></div>
-    </div>
-    <div class="main_bottom"></div>
-</div>
-
-
-<script>
-    <?php $this->beginBlock('editCustomerAddress') ?>
-    $(document).ready(function(){
-        $(".address_country").change(function(){
-            //alert(111);
-            ajaxurl = "<?= Yii::$service->url->getUrl('customer/address/changecountry') ?>";
-            country = $(this).val();
-            $.ajax({
-                async:false,
-                timeout: 8000,
-                dataType: 'json',
-                type:'get',
-                data: {
-                    'country':country,
-                },
-                url:ajaxurl,
-                success:function(data, textStatus){
-                    $(".state_html").html(data.state);
-                },
-                error:function (XMLHttpRequest, textStatus, errorThrown){
-
-                }
-            });
-
-        });
-
-    });
-    function submit_address(){
-        i = 1;
-        jQuery(".addressedit input").each(function(){
-            type = jQuery(this).attr("type");
-            if(type != "hidden"){
-                value = jQuery(this).val();
-                if(!value){
-                    //alert($(this).hasClass('optional'));
-                    if(!$(this).hasClass('optional')){
-                        i = 0;
+    public function initAddress()
+    {
+        $address = Yii::$app->request->post('address');
+        $redirect_url = Yii::$app->request->post('redirect_url');
+        $isSave = 0;
+        if (is_array($address) && !empty($address)) {
+            $address = \Yii::$service->helper->htmlEncode($address);
+            $this->save($address,$redirect_url);
+            $isSave = 1;
+        }
+        if (!$isSave) {
+            $this->_address_id = Yii::$app->request->get('address_id');
+            if ($this->_address_id) {
+                $addressModel = Yii::$service->customer->address->getByPrimaryKey($this->_address_id);
+                $identity = Yii::$app->user->identity;
+                $customer_id = $identity['id'];
+                if ($addressModel['address_id']) {
+                    // 该id必须是当前用户的
+                    if ($customer_id == $addressModel['customer_id']) {
+                        foreach ($addressModel as $k=>$v) {
+                            $this->_address[$k] = $v;
+                        }
                     }
                 }
             }
-        });
-
-        jQuery(".addressedit select").each(function(){
-            value = jQuery(this).val();
-            if(!value){
-                i = 0;
-            }
-        });
-        if(i){
-            jQuery(".addressedit").submit();
-        }else{
-            alert("You Must Fill All Field");
-            return false;
+        } else {
+            $this->_address = $address;
+        }
+        $country = isset($this->_address['country']) ? $this->_address['country'] : '';
+        if (!$country) {
+            $country = Yii::$service->helper->country->getDefaultCountry();
+        }
+        $this->_country = $country;
+        $this->getCountrySelect();
+        $this->getState();
+        if (!isset($this->_address['email']) || empty($this->_address['email'])) {
+            $identity = Yii::$app->user->identity;
+            $email = $identity['email'];
+            $this->_address['email'] = $email;
+        }
+        if (!isset($this->_address['first_name']) || empty($this->_address['first_name'])) {
+            $identity = Yii::$app->user->identity;
+            $first_name = $identity['firstname'];
+            $this->_address['first_name'] = $first_name;
+        }
+        if (!isset($this->_address['last_name']) || empty($this->_address['last_name'])) {
+            $identity = Yii::$app->user->identity;
+            $last_name = $identity['lastname'];
+            $this->_address['last_name'] = $last_name;
         }
     }
 
-    <?php $this->endBlock(); ?>
-    <?php $this->registerJs($this->blocks['editCustomerAddress'],\yii\web\View::POS_END);//将编写的js代码注册到页面底部 ?>
+    public function getLastData()
+    {
+        $this->initAddress();
+        if (empty($this->_address)) {
+            $this->_address = [];
+        }
+        $this->getIsDefault();
 
-</script>
+        return $this->_address;
+    }
 
+    public function getIsDefault()
+    {
+        $is_default_str = '';
+        $is_default = $this->_address['is_default'];
+        if (!$is_default) {
+            $address_id = $this->_address['address_id'];
+            if (!$address_id) {
+                $is_default_str = 'checked="checked"';
+            }
+        } else {
+            if ($is_default == 1) {
+                $is_default_str = 'checked="checked"';
+            }
+        }
+        $this->_address['is_default_str'] = $is_default_str;
+    }
 
+    public function getCountrySelect()
+    {
+        $countrySelect = Yii::$service->helper->country->getAllCountryOptions('', '', $this->_country);
+        $this->_address['countrySelect'] = $countrySelect;
+    }
 
+    public function getState($country = '')
+    {
+        $state = isset($this->_address['state']) ? $this->_address['state'] : '';
+        if (!$country) {
+            $country = $this->_country;
+        }
+        $stateHtml = Yii::$service->helper->country->getStateOptionsByContryCode($country, $state);
+        if (!$stateHtml) {
+            $stateHtml = '<input id="state" name="address[state]" value="'.$state.'" title="State" class="input-text" style="" type="text">';
+        } else {
+            $stateHtml = '<select id="address:state" class="address_state validate-select" title="State" name="address[state]">
+							<option value="">Please select region, state or province</option>'
+                        .$stateHtml.'</select>';
+        }
+        $this->_address['stateHtml'] = $stateHtml;
 
+        return $stateHtml;
+    }
 
+    public function getAjaxState()
+    {
+        $country = Yii::$app->request->get('country');
+        $state = $this->getState($country);
+        echo json_encode([
+            'state' => $state,
+        ]);
+        exit;
+    }
 
+    public function save($address,$redirect_url)
+    {
+        $arr = [];
+        $email = isset($address['email']) ? $address['email'] : '';
+        $first_name = isset($address['first_name']) ? $address['first_name'] : '';
+        $last_name = isset($address['last_name']) ? $address['last_name'] : '';
+        $telephone = isset($address['telephone']) ? $address['telephone'] : '';
+        $country = isset($address['country']) ? $address['country'] : '';
+        $state = isset($address['state']) ? $address['state'] : '';
+        //$company = isset($address['company']) ? $address['company'] : '';
+        //$fax = isset($address['fax']) ? $address['fax'] : '';
+        $street1 = isset($address['street1']) ? $address['street1'] : '';
+        $street2 = isset($address['street2']) ? $address['street2'] : '';
 
+        $city = isset($address['city']) ? $address['city'] : '';
+        $zip = isset($address['zip']) ? $address['zip'] : '';
+        $is_default = isset($address['is_default']) ? $address['is_default'] : '';
+//        if (!$email) {
+//            $error[] = ['email'];
+//        } else {
+//            $arr['email'] = $email;
+//        }
+        if (!$first_name) {
+            $error[] = ['first_name'];
+        } else {
+            $arr['first_name'] = $first_name;
+        }
+        if (!$last_name) {
+            $error[] = ['last_name'];
+        } else {
+            $arr['last_name'] = $last_name;
+        }
+        if (!$telephone) {
+            $error[] = ['telephone'];
+        } else {
+            $arr['telephone'] = $telephone;
+        }
+        if (!$country) {
+            $error[] = ['country'];
+        } else {
+            $arr['country'] = $country;
+        }
+        if (!$state) {
+            $error[] = ['state'];
+        } else {
+            $arr['state'] = $state;
+        }
+        if (!$street1) {
+            $error[] = ['street1'];
+        } else {
+            $arr['street1'] = $street1;
+        }
+        if (!$city) {
+            $error[] = ['city'];
+        } else {
+            $arr['city'] = $city;
+        }
+        if (!$zip) {
+            $error[] = ['zip'];
+        } else {
+            $arr['zip'] = $zip;
+        }
+        if (!empty($error)) {
+            $str = implode(',', $error).' can not empty';
+            Yii::$service->page->message->addError($str);
 
+            return;
+        }
+        if ($street2) {
+            $arr['street2'] = $street2;
+        }
+        if ($is_default) {
+            $arr['is_default'] = $is_default;
+        }
+        if ($is_default) {
+            $arr['is_default'] = $is_default;
+        }
+        if (isset($address['address_id'])) {
+            $arr['address_id'] = $address['address_id'];
+        }
 
-
+        $identity = Yii::$app->user->identity;
+        $arr['customer_id'] = $identity['id'];
+        Yii::$service->customer->address->save($arr);
+        if($redirect_url){
+            header('Location:'.$redirect_url);
+            exit();
+        }
+        return Yii::$service->url->redirectByUrlKey('customer/address');
+    }
+}
