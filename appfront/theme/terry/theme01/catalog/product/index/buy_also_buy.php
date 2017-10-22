3a4d87387af5b54eb5d463173dee0836b91ac9a0
USER-20150613FK\Administrator <jerry@2bai.com.cn>
2017-10-22 21:23:53 +0800

    commit

--- appfront/theme/terry/theme01/catalog/product/index/buy_also_buy.php
+++ appfront/theme/terry/theme01/catalog/product/index/buy_also_buy.php
@@ -12,7 +12,6 @@
     <fieldset class="sec">
         <legend><b class="px14 red_dark">Buy more related tools and Save more!</b></legend>
         <?php foreach($parentThis['products'] as $product): ?>
-        <?php var_dump($product);?>
         <div class="pro_list">
             <div class="photo"><div class="special"><img src="<?= Yii::$service->image->getImgUrl('images/lazyload1.gif','appfront') ; ?>" align="absmiddle" alt="Featured"></div>
                 <a href="<?= $product['url'] ?>" title="<?= $product['name'] ?>"><img src="<?= Yii::$service->product->image->getResize($product['image'],[120,120],false) ?>" width="120" height="120" border="0" hspace="0" vspace="0" alt="<?= $product['name'] ?>" align="absmiddle"></a></div>
@@ -39,7 +38,7 @@
                             ?>
                 <div class="blank10px"></div><span class="pro_b_item" id="id_pro_b_item_oQty_suit_48455"><b>Quantity: </b><input name="oQty_suit_48455" type="text" class="input" id="oQty_suit_48455" size="4" maxlength="6" onkeypress="event.returnValue=IsDigit();" value="1" onkeyup="ProQtySubTotal(this,'1','439.00','txt_single_subtotal_suit_48455');IsOrderNeedQty('Y','oQty_suit_48455');"><span class="px12"></span>　<b><span id="alert_o_need_oQty_suit_48455" class="alert"></span></b>　</span><span id="txt_single_subtotal_suit_48455" class="txt_subt_m"></span>
                 <div class="blank5px"></div>
-                <input name="add_to_cart" type="button" class="btn_addtocart_s" value="" title="Add to Cart" onclick="javascript:addProductToCart('<?= $product['_id'] ?>');return false;"/>
+                <input name="add_to_cart" type="button" class="btn_addtocart_s" value="" title="Add to Cart" onclick="javascript:addProductToCart('<?= $product['product_id'] ?>');return false;"/>
                 <div class="clear"></div></div>
             <div class="clear"></div>
         </div>
