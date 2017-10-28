f0ac501d53199c36c870d7d0e286e0286733ba81
iwisunny <iwisunny@gmail.com>
2017-10-28 17:09:33 +0800

    commit

--- appfront/theme/terry/theme01/catalog/product/index.php
+++ appfront/theme/terry/theme01/catalog/product/index.php
@@ -62,9 +62,6 @@
                         <div class="clear"></div>
                     </div>
                     <div class="blank10px"></div>
-                    <!--                <div class="exh_m_bri">Free Shipping Promotion for 2017 New & Hot sale Auto Key Programmer<br>Valid-->
-                    <!--                    time: 10th to 20th August, Shopping Now!-->
-                    <!--                </div>-->
                     <div class="blank10px"></div>
                     <div class="pro_bo_add_l"><input name="btn_buyitnow" type="button" class="btn_buyitnow" value="" title="Buy It Now"
                                                      onclick="location.href='<?= Yii::$service->url->getUrl('payment/paypal/express/start'); ?>'"/></div>
@@ -147,7 +144,20 @@
                 <?php if(!empty($tech_support)):?>
                 <li id="p_ab_mn_3" onclick="AreaMultiMenuShowHide('p_ab_mn_','p_ab_vw_',5,3,'current','');  GotoScrollTop('1', 'pro_ctab_star', 100, '', '', ''); ProTechService(8777,'p_ab_vw_xy_3');"><span>Tech Support</span></li>
                 <?php endif; ?>
-                <li id="p_ab_mn_4" onclick="AreaMultiMenuShowHide('p_ab_mn_','p_ab_vw_',5,4,'current','');  GotoScrollTop('1', 'pro_ctab_star', 100, '', '', '');"><span>Reviews<b id="num_pro_review_51561" class="red_dark"></b></span></li>
+                <li id="p_ab_mn_4" onclick="AreaMultiMenuShowHide('p_ab_mn_','p_ab_vw_',5,4,'current','');  GotoScrollTop('1', 'pro_ctab_star', 100, '', '', '');">
+                    <span>
+                        Reviews
+                        <?php # review部分。
+                        $reviewView = [
+                            'class' => 'fecshop\app\appfront\modules\Catalog\block\product\Review',
+                            'view' => 'catalog/product/index/review.php',
+                            'product_id' => $_id,
+                            'spu' => $spu,
+                        ];
+                        ?>
+                        <?= Yii::$service->page->widget->render($reviewView, $reviewParam); ?>
+                    </span>
+                </li>
                 <li><span><a href="<?= Yii::$service->url->getUrl('catalog/reviewproduct/add',['spu'=>$spu,'_id'=>$_id]); ?>">Write a Comment</a></span></li>
 <!--                <li onclick="GotoScrollTop('1', 'na_pro_releated', 100, '', '', '');"><span>Related Products</span></li>-->
                 <li id="p_ab_mn_5" onclick="AreaMultiMenuShowHide('p_ab_mn_','p_ab_vw_',5,5,'current',''); GotoScrollTop('1', 'pro_ctab_star', 100, '', '', '');"><span>After-sales Service</span></li>
