5cc58582b067b09442233c43c0ed60417f102700
iwisunny <iwisunny@gmail.com>
2017-11-07 17:26:17 +0800

    修改西联

--- appfront/theme/terry/theme01/checkout/onepage/index.php
+++ appfront/theme/terry/theme01/checkout/onepage/index.php
@@ -87,7 +87,7 @@
                         <div class="blank5px"></div>
 
                         <label for="Western Union">
-                            <input name="payment_method" type="radio" id="Western Union" value="Western Union"  onclick="javascript:AreaMultiShowHide('area_pay_method_exp_',3,2);">
+                            <input name="payment_method" type="radio" id="Western Union" value="WesternUnion"  onclick="javascript:AreaMultiShowHide('area_pay_method_exp_',3,2);">
                             <img src="<?= Yii::$service->image->getImgUrl('images/pay/ico_western_union.gif'); ?>"   alt="Western Union" border="0" align="absmiddle">&nbsp;&nbsp;<b class="px13 verdana">Western Union</b>
                             <input name="" type="submit" value="submit" class="ipt_img onestepcheckout-button" id="onestepcheckout-place-order">
                         </label>
