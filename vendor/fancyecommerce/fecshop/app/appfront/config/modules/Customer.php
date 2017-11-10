0c4a2db55affc83c52cee7f41db361ae9e8337a3
iwisunny <iwisunny@gmail.com>
2017-11-10 17:33:18 +0800

    修改

--- vendor/fancyecommerce/fecshop/app/appfront/config/modules/Customer.php
+++ vendor/fancyecommerce/fecshop/app/appfront/config/modules/Customer.php
@@ -43,13 +43,6 @@ return [
             ],
             // 在账户中心左侧栏显示的菜单。
             'leftMenu'  => [
-                'View My Orders'             => 'customer/order',
-               // 'Account Dashboard'     => 'customer/account',
-                'Account Information'   => 'customer/editaccount',
-                'Manage Address Book'          => 'customer/address',
-                "My Products' Reviews"    => 'customer/productreview',
-                'My Favorite'           => 'customer/productfavorite',
-
             ],
 
             'contacts'    => [
