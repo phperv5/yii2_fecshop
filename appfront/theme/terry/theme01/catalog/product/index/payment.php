918b71abe9ccb8d005432e1840119dd0ec92e86c
iwisunny <iwisunny@gmail.com>
2017-11-10 15:58:02 +0800

    修改地址

--- appfront/theme/terry/theme01/catalog/product/index/payment.php
+++ appfront/theme/terry/theme01/catalog/product/index/payment.php
@@ -13,15 +13,11 @@
 
 <p><?= Yii::$service->page->translate->__('1. PayPal'); ?></p>
 
-<p><img alt="" height="96" src="<?= Yii::$service->image->getImgUrl('images/paypal48.jpg','appfront') ?>" width="300"></p>
-
 <p><?= Yii::$service->page->translate->__('1) Login To Your Account or use Credit Card Express.'); ?><br>
 <?= Yii::$service->page->translate->__('2) Enter your Card Details, the order will be shipped to your PayPal address. And click "Submit".'); ?><br>
 <?= Yii::$service->page->translate->__('3) Your Payment will be processed and a receipt will be sent to your email inbox.'); ?></p>
 
 <p><?= Yii::$service->page->translate->__('2. Credit Card'); ?></p>
-
-<p><img alt="" height="40" src="<?= Yii::$service->image->getImgUrl('images/creditcard48.jpg','appfront') ?>" width="554"></p>
 
 <p>	<?= Yii::$service->page->translate->__('1) Choose your shipping address OR create a new one.'); ?><br>
 	<?= Yii::$service->page->translate->__('2) Enter your Card Details and click "Submit".'); ?><br>
