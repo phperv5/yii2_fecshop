8430b420fb83392634daa75d581174e24a5470b3
iwisunny <iwisunny@gmail.com>
2017-11-13 15:16:40 +0800

    commit

--- vendor/fancyecommerce/fecshop/app/appadmin/modules/Catalog/block/productinfo/Imageupload.php
+++ vendor/fancyecommerce/fecshop/app/appadmin/modules/Catalog/block/productinfo/Imageupload.php
@@ -73,7 +73,7 @@ class Imageupload
     {
         $root_path = '../../';
         require $root_path . 'src/UploadFile.php';
-        $file_path = Yii::$service->image->imgUrl('attachment/');
+        $file_path = Yii::$service->image->getImgUrl('attachment/');
         $upload = new \UploadFile();
         $upload->savePath = $file_path;// 设置附件上传目录   默认上传目录为 ./uploads/
 
