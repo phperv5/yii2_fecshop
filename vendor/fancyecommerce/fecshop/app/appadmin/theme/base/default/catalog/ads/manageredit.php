477bd27965e81c72cfd7c2d48408f1c7fda3017a
iwisunny <iwisunny@gmail.com>
2017-11-19 15:56:48 +0800

    commit

--- vendor/fancyecommerce/fecshop/app/appadmin/theme/base/default/catalog/ads/manageredit.php
+++ vendor/fancyecommerce/fecshop/app/appadmin/theme/base/default/catalog/ads/manageredit.php
@@ -10,36 +10,45 @@ use yii\helpers\Html;
 use fec\helpers\CRequest;
 use fecadmin\models\AdminRole;
 use fec\helpers\CUrl;
+
 ?>
 ?>
 <style>
-.checker{float:left;}
-.dialog .pageContent {background:none;}
-.dialog .pageContent .pageFormContent{background:none;}
+    .checker {
+        float: left;
+    }
+
+    .dialog .pageContent {
+        background: none;
+    }
+
+    .dialog .pageContent .pageFormContent {
+        background: none;
+    }
 </style>
 
-<div class="pageContent"> 
-	<form  method="post" action="<?= $saveUrl ?>" class="pageForm required-validate" onsubmit="return validateCallback(this, dialogAjaxDoneCloseAndReflush);">
-		<?php echo CRequest::getCsrfInputHtml();  ?>	
-		<div layouth="56" class="pageFormContent" style="height: 240px; overflow: auto;">
-				<fieldset id="fieldset_table_qbe">
-					<legend style="color:#cc0000">编辑信息</legend>
-					<div>
-						<?= $editBar; ?>
-					</div>
-				</fieldset>
+<div class="pageContent">
+    <form method="post" action="<?= $saveUrl ?>" class="pageForm required-validate"
+          onsubmit="return validateCallback(this, dialogAjaxDoneCloseAndReflush);">
+        <?php echo CRequest::getCsrfInputHtml(); ?>
+        <div layouth="56" class="pageFormContent" style="height: 240px; overflow: auto;">
+            <fieldset id="fieldset_table_qbe">
+                <legend style="color:#cc0000">编辑信息</legend>
+                <div>
+                    <?= $editBar; ?>
+                </div>
+            </fieldset>
             <div>
-
-                <button style="" onclick="getElementById('attachfile').click()" class="scalable" type="button" title="Duplicate" id=""><span><span><span>附件上传</span></span></span></button>
-                <input type="file" multiple="multiple" id="attachfile" style="height:0;width:0;z-index: -1; position: absolute;left: 10px;top: 5px;"/>
+                <table  class="productattach">
+                    <tbody>
+                    </tbody>
+                </table>
+                <button style="" onclick="getElementById('attachfile').click()" class="scalable" type="button"
+                        title="Duplicate" id=""><span><span><span>上传图片</span></span></span></button>
+                <input type="file" multiple="multiple" id="attachfile"
+                       style="height:0;width:0;z-index: -1; position: absolute;left: 10px;top: 5px;"/>
                 <script>
                     jQuery(document).ready(function () {
-                        jQuery("body").on('click', ".attachment_delete_img", function () {
-                            jQuery(this).parent().parent().remove();
-                        });
-                        //jQuery(".delete_img").click(function(){
-                        //	jQuery
-                        //});
 
                         //响应文件添加成功事件
                         $("#attachfile").change(function () {
@@ -83,8 +92,8 @@ use fec\helpers\CUrl;
                                     //else $("#feedback").children('img').eq(0).before(data.replace(/&lt;/g,'<').replace(/&gt;/g,'>'));
                                     //	alert(data.return_status);
                                     if (data.return_status == "success") {
-                                        jQuery(".productattach tbody ").append(data.img_str);
-                                    }else{
+                                        jQuery(".productattach tbody ").empty().append(data.img_str);
+                                    } else {
                                         alert(data.msg);
                                     }
                                 },
@@ -97,18 +106,28 @@ use fec\helpers\CUrl;
                     });
                 </script>
             </div>
-		</div>
-	
-		<div class="formBar">
-			<ul>
-				<!--<li><a class="buttonActive" href="javascript:;"><span>保存</span></a></li>-->
-				<li><div class="buttonActive"><div class="buttonContent"><button onclick="func('accept')"  value="accept" name="accept" type="submit">保存</button></div></div></li>
-			
-				<li>
-					<div class="button"><div class="buttonContent"><button type="button" class="close">取消</button></div></div>
-				</li>
-			</ul>
-		</div>
-	</form>
+        </div>
+
+        <div class="formBar">
+            <ul>
+                <!--<li><a class="buttonActive" href="javascript:;"><span>保存</span></a></li>-->
+                <li>
+                    <div class="buttonActive">
+                        <div class="buttonContent">
+                            <button onclick="func('accept')" value="accept" name="accept" type="submit">保存</button>
+                        </div>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="button">
+                        <div class="buttonContent">
+                            <button type="button" class="close">取消</button>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </form>
 </div>	
 
