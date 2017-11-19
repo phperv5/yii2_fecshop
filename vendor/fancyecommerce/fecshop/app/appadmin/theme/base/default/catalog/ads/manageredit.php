e2a763954db677bfe6c625236c73da6cf609ab45
iwisunny <iwisunny@gmail.com>
2017-11-19 16:17:36 +0800

    commit

--- vendor/fancyecommerce/fecshop/app/appadmin/theme/base/default/catalog/ads/manageredit.php
+++ vendor/fancyecommerce/fecshop/app/appadmin/theme/base/default/catalog/ads/manageredit.php
@@ -37,77 +37,77 @@ use fec\helpers\CUrl;
                 <div>
                     <?= $editBar; ?>
                 </div>
-                <div>
-                    <table  class="productattach">
-                        <tbody>
-                        </tbody>
-                    </table>
-                    <button style="" onclick="getElementById('attachfile').click()" class="scalable" type="button"
-                            title="Duplicate" id=""><span><span><span>上传图片</span></span></span></button>
-                    <input type="file" multiple="multiple" id="attachfile"
-                           style="height:0;width:0;z-index: -1; position: absolute;left: 10px;top: 5px;"/>
-                    <script>
-                        jQuery(document).ready(function () {
+            </fieldset>
+            <div>
+                <table  class="productattach">
+                    <tbody>
+                    </tbody>
+                </table>
+                <button style="" onclick="getElementById('attachfile').click()" class="scalable" type="button"
+                        title="Duplicate" id=""><span><span><span>上传图片</span></span></span></button>
+                <input type="file" multiple="multiple" id="attachfile"
+                       style="height:0;width:0;z-index: -1; position: absolute;left: 10px;top: 5px;"/>
+                <script>
+                    jQuery(document).ready(function () {
 
-                            //响应文件添加成功事件
-                            $("#attachfile").change(function () {
+                        //响应文件添加成功事件
+                        $("#attachfile").change(function () {
 
-                                //创建FormData对象
-                                var thisindex = 0;
-                                jQuery(".productattach tbody tr").each(function () {
-                                    rel = parseInt(jQuery(this).attr("rel"));
-                                    //alert(rel);
-                                    if (rel > thisindex) {
-                                        thisindex = rel;
+                            //创建FormData对象
+                            var thisindex = 0;
+                            jQuery(".productattach tbody tr").each(function () {
+                                rel = parseInt(jQuery(this).attr("rel"));
+                                //alert(rel);
+                                if (rel > thisindex) {
+                                    thisindex = rel;
+                                }
+                            });
+                            //alert(thisindex);
+                            var data = new FormData();
+                            data.append('thisindex', thisindex);
+
+                            //为FormData对象添加数据
+                            $.each($('#attachfile')[0].files, function (i, file) {
+                                data.append('upload_file' + i, file);
+                            });
+                            //$(".loading").show();	//显示加载图片
+                            //发送数据
+
+
+                            $.ajax({
+                                url: '<?= CUrl::getUrl('catalog/productinfo/imageupload')  ?>',
+                                type: 'POST',
+                                data: data,
+                                async: false,
+                                dataType: 'json',
+                                timeout: 80000,
+                                cache: false,
+                                contentType: false,		//不可缺参数
+                                processData: false,		//不可缺参数
+                                success: function (data, textStatus) {
+                                    //data = $(data).html();
+                                    //第一个feedback数据直接append，其他的用before第1个（ .eq(0).before() ）放至最前面。
+                                    //data.replace(/&lt;/g,'<').replace(/&gt;/g,'>') 转换html标签，否则图片无法显示。
+                                    //if($("#feedback").children('img').length == 0) $("#feedback").append(data.replace(/&lt;/g,'<').replace(/&gt;/g,'>'));
+                                    //else $("#feedback").children('img').eq(0).before(data.replace(/&lt;/g,'<').replace(/&gt;/g,'>'));
+                                    //	alert(data.return_status);
+                                    if (data.return_status == "success") {
+                                        var str = '<img src="'+data.img_url+'" style="width: 100px;"/>'
+                                        jQuery(".productattach tbody ").empty().append(str);
+                                        $('[name="editFormData[banner]"]').val(data.img_url);
+                                    } else {
+                                        alert(data.msg);
                                     }
-                                });
-                                //alert(thisindex);
-                                var data = new FormData();
-                                data.append('thisindex', thisindex);
-
-                                //为FormData对象添加数据
-                                $.each($('#attachfile')[0].files, function (i, file) {
-                                    data.append('upload_file' + i, file);
-                                });
-                                //$(".loading").show();	//显示加载图片
-                                //发送数据
-
-
-                                $.ajax({
-                                    url: '<?= CUrl::getUrl('catalog/productinfo/imageupload')  ?>',
-                                    type: 'POST',
-                                    data: data,
-                                    async: false,
-                                    dataType: 'json',
-                                    timeout: 80000,
-                                    cache: false,
-                                    contentType: false,		//不可缺参数
-                                    processData: false,		//不可缺参数
-                                    success: function (data, textStatus) {
-                                        //data = $(data).html();
-                                        //第一个feedback数据直接append，其他的用before第1个（ .eq(0).before() ）放至最前面。
-                                        //data.replace(/&lt;/g,'<').replace(/&gt;/g,'>') 转换html标签，否则图片无法显示。
-                                        //if($("#feedback").children('img').length == 0) $("#feedback").append(data.replace(/&lt;/g,'<').replace(/&gt;/g,'>'));
-                                        //else $("#feedback").children('img').eq(0).before(data.replace(/&lt;/g,'<').replace(/&gt;/g,'>'));
-                                        //	alert(data.return_status);
-                                        if (data.return_status == "success") {
-                                            var str = '<img src="'+data.img_url+'" style="width: 100px;"/>'
-                                            jQuery(".productattach tbody ").empty().append(str);
-                                            $('[name="editFormData[banner]"]').val(data.img_url);
-                                        } else {
-                                            alert(data.msg);
-                                        }
-                                    },
-                                    error: function () {
-                                        alert('上传出错');
-                                        //$(".loading").hide();	//加载失败移除加载图片
-                                    }
-                                });
+                                },
+                                error: function () {
+                                    alert('上传出错');
+                                    //$(".loading").hide();	//加载失败移除加载图片
+                                }
                             });
                         });
-                    </script>
-                </div>
-            </fieldset>
+                    });
+                </script>
+            </div>
         </div>
 
         <div class="formBar">
