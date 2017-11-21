467c46e9875f5d111c21775aa6cc49d6e99cec56
USER-20150613FK\Administrator <jerry@2bai.com.cn>
2017-11-21 22:38:49 +0800

    commit

--- vendor/fancyecommerce/fecshop/app/appadmin/modules/Catalog/block/newarrivals/Manageredit.php
+++ vendor/fancyecommerce/fecshop/app/appadmin/modules/Catalog/block/newarrivals/Manageredit.php
@@ -20,7 +20,7 @@ use Yii;
  * @author Terry Zhao <2358269014@qq.com>
  * @since 1.0
  */
-class Manageredit extends AppadminbaseBlockEdit implements AppadminbaseBlockInterface
+class Manageredit extends AppadminbaseBlockEdit implements AppadminbaseBlockEditInterface
 {
     public $_saveUrl;
 
