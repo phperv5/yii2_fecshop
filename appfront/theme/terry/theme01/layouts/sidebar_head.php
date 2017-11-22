71168ea83023b395c88cc442f6e0b40bbb218d02
USER-20150613FK\Administrator <jerry@2bai.com.cn>
2017-11-22 22:51:43 +0800

    commit

---
+++ appfront/theme/terry/theme01/layouts/sidebar_head.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+?>
+<?php
+$jsOptions = [
+	# js config 1
+	[
+		'options' => [
+			'position' =>  'POS_END',
+		],
+		'js'	=>[
+			'js/jquery-3.0.0.min.js',
+		],
+	],
+];
+
+# css config
+$cssOptions = [
+	# css config 1.
+	[
+		'css'	=>[
+			'css/sidebar.css',
+		],
+	],
+];
+\Yii::$service->page->asset->jsOptions 	= $jsOptions;
+\Yii::$service->page->asset->cssOptions = $cssOptions;				
+\Yii::$service->page->asset->register($this);
+?>
+
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+	<div class="main-container">
+		<?= $content; ?>
+	</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
+
