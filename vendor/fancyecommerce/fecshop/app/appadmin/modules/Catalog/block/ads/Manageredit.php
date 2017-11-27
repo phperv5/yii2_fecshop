49a4faac8f2aa42a6de6d67b0a29ef0b1009e672
iwisunny <iwisunny@gmail.com>
2017-11-27 14:04:24 +0800

    commit

--- vendor/fancyecommerce/fecshop/app/appadmin/modules/Catalog/block/ads/Manageredit.php
+++ vendor/fancyecommerce/fecshop/app/appadmin/modules/Catalog/block/ads/Manageredit.php
@@ -67,7 +67,7 @@ class Manageredit extends AppadminbaseBlockEdit implements AppadminbaseBlockEdit
                 'require' => 1,
             ],
             [
-                'label'=>'banner图（960*360）',
+                'label'=>'banner(960*360)',
                 'name'=>'banner',
                 'width' => 500,
                 'display'=>[
