23a2a873fc56a613c634d5fd75f2c8fcb1140983
iwisunny <iwisunny@gmail.com>
2017-11-29 16:31:29 +0800

    修改

--- vendor/fancyecommerce/fecshop/app/appadmin/config/params.php
+++ vendor/fancyecommerce/fecshop/app/appadmin/config/params.php
@@ -5,20 +5,4 @@ return [
     'appadminBaseTheme'     => '@fecshop/app/appadmin/theme/base/default',
     'appadminBaseLayoutName'=> 'main_admin.php',
     'appName'               => 'appadmin',
-    'bootstrap' => [
-        'queue', // The component registers own console commands
-    ],
-    'components' => [
-        'db' => [
-            'class' => \yii\db\Connection::class,
-        ],
-        'queue' => [
-            'class' => \yii\queue\db\Queue::class,
-            'db' => 'db', // DB connection component or its config
-            'tableName' => 'queue', // Table name
-            'channel' => 'default', // Queue channel key
-            'mutex' => \yii\mutex\MysqlMutex::class, // Mutex that used to sync queries
-        ],
-    ],
-
 ];
