53088378f0d0edf04f3d5431cf990abe6bf71373
iwisunny <iwisunny@gmail.com>
2017-12-01 10:55:22 +0800

    修改

--- vendor/fancyecommerce/fecshop/app/appadmin/modules/Catalog/block/productinfo/Imageupload.php
+++ vendor/fancyecommerce/fecshop/app/appadmin/modules/Catalog/block/productinfo/Imageupload.php
@@ -86,6 +86,7 @@ class Imageupload
             $fileInfo = $upload->getUploadFileInfo();
         }
         $fileInfo = $fileInfo[0];
+        var_dump($fileInfo);die;
         $str = '<tr class="p_img" data="'.$fileInfo['savename'].'"  style="border-bottom:1px solid #ccc;">
 									<td style="width:120px;text-align:center;"><a href="'.Yii::$service->image->getImgUrl('attachment/'.$fileInfo['savename']).'">'.$fileInfo['oldname'].'</a> </td>
 									<td style="padding:0 0 0 20px;"><a class="attachment_delete_img btnDel" href="javascript:void(0)">删除</a></td>
