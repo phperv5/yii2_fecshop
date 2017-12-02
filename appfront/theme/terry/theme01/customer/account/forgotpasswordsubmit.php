248ed1ef1716359d58063375850830e9c16778a4
iwisunny <iwisunny@gmail.com>
2017-12-02 16:29:23 +0800

    修改

--- appfront/theme/terry/theme01/customer/account/forgotpasswordsubmit.php
+++ appfront/theme/terry/theme01/customer/account/forgotpasswordsubmit.php
@@ -1,17 +1,21 @@
-
 <div class="main">
     <div class="exh_full_top"></div>
     <div class="exh_full_main">
         <h1>Forget Password?</h1>
 
-        <div class="blank10px"></div><div class="blank10px"></div>
-        <div class="w95per_fr"><div class="w95per_fr">
+        <div class="blank10px"></div>
+        <div class="blank10px"></div>
+        <div class="w95per_fr">
+            <div class="w95per_fr">
                 <b class="gray">Step 1: Please Input Your E-mail Address and Verification Code</b>
                 <div class="blank5px"></div>
                 <b class="red">Step 2: Check Your E-mail Box and Get Your Password</b>
                 <div class="blank5px"></div>
-            </div></div>
-        <div class="blank10px"></div><div class="blank10px"></div><div class="blank10px"></div>
+            </div>
+        </div>
+        <div class="blank10px"></div>
+        <div class="blank10px"></div>
+        <div class="blank10px"></div>
 
         <div class="align_center px14 red verdana"></div>
 
@@ -19,31 +23,27 @@
         <div class="blank10px"></div>
         <div class="blank10px"></div>
 
-        <div class="align_center"><br /><br /><br /><br />
+        <div class="align_center"><br/><br/><br/><br/>
 
-
-
-
-
-
-            <div class="align_center px16 red verdana"><span id="sRtnGetPwdInfo"></span></div><br /><br /><br />
-
-            <script type="text/javascript">
-                MailRelaySend( '1', 'sRtnGetPwdInfo', '312043814@qq.com', 'zewe jewew', '', 'UserGetLoginInfo', '351041', '' );
-            </script>
-
-
-
-            <div class="blank10px"></div><div class="blank10px"></div><div class="blank10px"></div>
-            <input name="Submit2" type="button" class="btn_submit btn_mid" value="  Go Back  " onclick="javascript:window.location='http://www.uobdii.com/app/signin.asp';" />
-            <div class="blank10px"></div><div class="blank10px"></div>
+            <div class="align_center px16 red verdana">
+                <span id="sRtnGetPwdInfo">
+                    <b class="green">An email sent to <i><?=  $identity['email'] ?></i> successfully. Please check.</b>
+                </span>
+            </div>
+            <br/><br/><br/>
+            <div class="blank10px"></div>
+            <div class="blank10px"></div>
+            <div class="blank10px"></div>
+            <input name="Submit2" type="button" class="btn_submit btn_mid" value="  Go Back  "
+                   onclick="javascript:window.location='http://www.uobdii.com/app/signin.asp';"/>
+            <div class="blank10px"></div>
+            <div class="blank10px"></div>
 
 
         </div>
         <div class="blank10px"></div>
         <div class="blank10px"></div>
         <div class="blank10px"></div>
-
 
 
     </div>
