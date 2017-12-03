1a7fba565a22ca1d0136942ec1826e1bba97e656
USER-20150613FK\Administrator <jerry@2bai.com.cn>
2017-12-03 18:50:59 +0800

    commit

--- apphtml5/theme/terry/theme01/catalog/category/index.php
+++ apphtml5/theme/terry/theme01/catalog/category/index.php
@@ -1,30 +1,21 @@
-
-<h1>Auto Key Programmer</h1>
-<div class="pro_list" id="pro_list_71861">
+<h1><?= $name ?></h1>
+            <?php if (is_array($products) && !empty($products)): ?>
+                <?php foreach ($products as $product): ?>
+<div class="pro_list" id="">
     <div class="pro_li_photo">
         <div class="pro_li_off">9% off</div>
-        <a href="/wholesale/yanhua-bmw-fem-key-programmer.html"
-           title="2017 Latest Yanhua BMW FEM/BDC Key Programmer Free Shipping by DHL"><img
-                src="http://www.UOBDII.com/upload/pro/yanhua-bmw-fem-key-programmer-180.jpg" width="120" height="120"
-                border="0" hspace="0" vspace="0"
-                alt="2017 Latest Yanhua BMW FEM/BDC Key Programmer Free Shipping by DHL" align="absmiddle"></a></div>
+        <a href="<?= $product['url'] ?>" title="<?= $product['name'] ?>">
+            <img src="<?= Yii::$service->product->image->getResize($product['image'], [120, 120], false) ?>" width="120" height="120" border="0" hspace="0" vspace="0" alt="<?= $product['name'] ?>" align="absmiddle">
+        </a>
+    </div>
     <div class="pro_li_brief">
 
-        <h2><a href="/wholesale/yanhua-bmw-fem-key-programmer.html">
-            <span class="iconfont icon-new gold font_large no_bold"></span> 2017 Latest Yanhua BMW FEM/BDC Key
-            Programmer Free Shipping by DHL</a></h2>
-
+        <h2><a href="<?= $product['url'] ?>">
+            <span class="iconfont icon-new gold font_large no_bold"></span><?= $product['name'] ?></a></h2>
         <div class="clear"></div>
-
-
-        <span class="font_medium">No.SK226</span>&nbsp;&nbsp;
-
+        <span class="font_medium">No.<?= $product['sku']; ?></span>&nbsp;&nbsp;
         <b class="font_medium green">Free Shipping</b> <span class="iconfont icon-localshipping green"></span>
-
-
         <div class="blank5px"></div>
-
-
         <span name="cc_v_USD" style="display:"><span class="pri_pg_vip">$529.00</span><span class="pri_pg_sale"><i>$579.00</i></span></span>
         <span name="cc_v_EUR" style="display:none"><span class="pri_pg_vip">€449.65</span><span class="pri_pg_sale"><i>€492.15</i></span></span>
         <span name="cc_v_GBP" style="display:none"><span class="pri_pg_vip">£412.62</span><span class="pri_pg_sale"><i>£451.62</i></span></span>
@@ -49,142 +40,5 @@
 
     <div class="clear"></div>
 </div>
-
-
-<div class="pro_list" id="pro_list_48436">
-    <div class="pro_li_photo"><a href="/wholesale/vvdi2-with-basic-bmw-and-obd-functions.html"
-                                 title="Original Xhorse VVDI2 Commander Key Programmer With Basic, BMW and OBD Functions, Newly Add BMW FEM/BDC Function"><img
-            src="http://www.UOBDII.com/upload/pro/vvdi2-with-basic-bmw-and-obd-functions-180.3.jpg" width="120"
-            height="120" border="0" hspace="0" vspace="0"
-            alt="Original Xhorse VVDI2 Commander Key Programmer With Basic, BMW and OBD Functions, Newly Add BMW FEM/BDC Function"
-            align="absmiddle"></a></div>
-    <div class="pro_li_brief">
-
-        <h2><a href="/wholesale/vvdi2-with-basic-bmw-and-obd-functions.html">
-            <span class="iconfont icon-hot red font_large no_bold"></span><span
-                class="iconfont icon-countdown blue font_large no_bold"></span> Original Xhorse VVDI2 Commander Key
-            Programmer With Basic, BMW and OBD Functions, Newly Add BMW FEM/BDC Function</a></h2>
-
-        <div class="clear"></div>
-
-
-        <span class="font_medium">No.SV86-D</span>&nbsp;&nbsp;
-
-        <b class="font_medium green">Free Shipping</b> <span class="iconfont icon-localshipping green"></span>
-
-
-        <div class="blank5px"></div>
-
-
-        <span name="cc_v_USD" style="display:"><span class="pri_pg_vip">$1,029.00</span></span>
-        <span name="cc_v_EUR" style="display:none"><span class="pri_pg_vip">€874.65</span></span>
-        <span name="cc_v_GBP" style="display:none"><span class="pri_pg_vip">£802.62</span></span>
-        <span name="cc_v_AUD" style="display:none"><span class="pri_pg_vip">AU$1,317.12</span></span>
-        <span name="cc_v_JPY" style="display:none"><span class="pri_pg_vip">¥114,219</span></span>
-
-        <div class="blank5px"></div>
-        <a href="/reviews/pro48436">
-            <div class="rate_star_w75">
-                <div class="rate_star_w75_bg">
-                    <div class="rate_star_w75_vw" style="width:75px;"></div>
-                </div>
-            </div>
-            <div class="rate_star_w75_tx">5 reviews</div>
-        </a>
-        <div class="clear"></div>
-
-
-        <div class="clear"></div>
-    </div>
-
-    <div class="clear"></div>
-</div>
-
-
-<div class="pro_list" id="pro_list_65786">
-    <div class="pro_li_photo">
-        <div class="pro_li_off">12% off</div>
-        <a href="/wholesale/keydiy-kd900-for-ios-android-bluetooth-remote-maker.html"
-           title="Original KEYDIY KD900+ Mobile Remote Key Generator Best Tool for Remote Control"><img
-                src="http://www.UOBDII.com/upload/pro/keydiy-kd900-for-ios-android-bluetooth-remote-maker-180.jpg"
-                width="120" height="120" border="0" hspace="0" vspace="0"
-                alt="Original KEYDIY KD900+ Mobile Remote Key Generator Best Tool for Remote Control" align="absmiddle"></a>
-    </div>
-    <div class="pro_li_brief">
-
-        <h2><a href="/wholesale/keydiy-kd900-for-ios-android-bluetooth-remote-maker.html">
-            Original KEYDIY KD900+ Mobile Remote Key Generator Best Tool for Remote Control</a></h2>
-
-        <div class="clear"></div>
-
-
-        <span class="font_medium">No.SK216</span>&nbsp;&nbsp;
-
-
-        <div class="blank5px"></div>
-
-
-        <span name="cc_v_USD" style="display:"><span class="pri_pg_vip">$229.00</span><span class="pri_pg_sale"><i>$259.00</i></span></span>
-        <span name="cc_v_EUR" style="display:none"><span class="pri_pg_vip">€194.65</span><span class="pri_pg_sale"><i>€220.15</i></span></span>
-        <span name="cc_v_GBP" style="display:none"><span class="pri_pg_vip">£178.62</span><span class="pri_pg_sale"><i>£202.02</i></span></span>
-        <span name="cc_v_AUD" style="display:none"><span class="pri_pg_vip">AU$293.12</span><span
-                class="pri_pg_sale"><i>AU$331.52</i></span></span>
-        <span name="cc_v_JPY" style="display:none"><span class="pri_pg_vip">¥25,419</span><span class="pri_pg_sale"><i>¥28,749</i></span></span>
-
-        <div class="blank5px"></div>
-        <a href="/reviews/pro65786">
-            <div class="rate_star_w75">
-                <div class="rate_star_w75_bg">
-                    <div class="rate_star_w75_vw" style="width:75px;"></div>
-                </div>
-            </div>
-            <div class="rate_star_w75_tx">4 reviews</div>
-        </a>
-        <div class="clear"></div>
-
-
-        <div class="clear"></div>
-    </div>
-
-    <div class="clear"></div>
-</div>
-
-
-<div class="page_nav">
-    <div class="pgn_next"><a href="/wholesale/auto-key-programmer/p2/" title="Next"><span
-            class="iconfont icon-right font_large"></span></a></div>
-    Page <span class="red_dark">1</span> of 9
-    <div class="clear"></div>
-</div>
-<div class="pro_chf font_small line15em">Popular Search: <a href="/producttags/vvdi2.html"><strong>VVDI2</strong></a>&nbsp;&nbsp;
-    <a href="/producttags/mb-bga-tool.html">MB BGA Tool</a>&nbsp;&nbsp;
-    <a href="/producttags/keydiy-remote-maker.html"><strong>KEYDIY Remote Maker</strong></a>&nbsp;&nbsp;
-    <a href="/producttags/bmw-fem-key-programmer.html">BMW FEM Key Programmer</a>&nbsp;&nbsp;
-    <a href="/producttags/cgdi-prog-key-programmer.html"><strong>CGDI Prog Key Programmer</strong></a>&nbsp;&nbsp;
-    <a href="/producttags/yh-bmw-fem.html">YH BMW FEM</a>&nbsp;&nbsp;
-    <a href="/producttags/universal-key-programmer.html"><strong>Universal Key Programmer</strong></a>&nbsp;&nbsp;
-    <a href="/producttags/pin-code-reader.html">Pin Code Reader</a>&nbsp;&nbsp;
-    <a href="/producttags/toyota-g-chip-copier.html"><strong>Toyota G Chip Copier</strong></a>&nbsp;&nbsp;
-    <div class="clear"></div>
-</div>
-
-
-<div class="m_row_u">
-    <ul>
-
-        <li id="fta_user"><i><a href="/members/"><span class="iconfont icon-my"></span> Sign In / Register</a></i></li>
-
-        <li><a href="/professional_customer_service.html"><span class="iconfont icon-info"></span> Customer Service</a>
-        </li>
-        <a href="/professional_customer_service.html">
-        </a>
-        <li><a href="/professional_customer_service.html"></a><a href="/support/"><span
-                class="iconfont icon-question"></span> Help</a></li>
-        <li class="mru_l_last" id="fta_curr_country"><a href="javascript:void(0);"
-                                                        onclick="javascript:CountryCurrencyChoose('Choose','');"><span
-                class="iconfont icon-refresh"></span> <i class="font_medium">Currency: </i>USD &nbsp;&nbsp; <i
-                class="font_medium">Ship to</i> <img src="/images/ico_country/CN.gif" border="0" align="absmiddle"></a>
-        </li>
-    </ul>
-</div>
-
+<?php endforeach; ?>
+<?php endif; ?>
