a255fe61c39af4e6911b4e232d86295b99774499
USER-20150613FK\Administrator <jerry@2bai.com.cn>
2017-12-03 15:56:41 +0800

    commit

---
+++ common/config/fecshop_local_services/Cms.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ *
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+return [
+    'cms' => [
+        'params'=> [
+            'home_title'            => [
+                'home_title_en' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+                'home_title_zh' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+                'home_title_fr' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+                'home_title_es' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+            ],
+            'home_meta_keywords'            => [
+                'home_meta_keywords_en' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+                'home_meta_keywords_zh' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+                'home_meta_keywords_fr' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+                'home_meta_keywords_es' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+            ],
+
+            'home_meta_description'            => [
+                'home_meta_description_en' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+                'home_meta_description_zh' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+                'home_meta_description_fr' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+                'home_meta_description_es' => 'Norman-Keys, Locksmith Tools Golden Supplier',
+            ],
+            'homeBestSellerSku'        => [
+                '10001', '10002', '10003', '10004','10005'
+            ],
+            'homeFeaturedSku'        => [
+                '10001', '10002',
+            ],
+
+        ],
+    ],
+];
