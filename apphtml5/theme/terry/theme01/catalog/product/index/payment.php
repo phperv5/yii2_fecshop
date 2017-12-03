d6fa34d3f9873e72db0e9a4172d5c4bad13508ef
USER-20150613FK\Administrator <jerry@2bai.com.cn>
2017-12-03 15:34:19 +0800

    commit

---
+++ apphtml5/theme/terry/theme01/catalog/product/index/payment.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+?>
+<p><?= Yii::$service->page->translate->__('FECSHOP.com accepts PayPal, Credit Card, Western Union and Wire Transfer as secure payment methods:'); ?></p>
+
+<p><?= Yii::$service->page->translate->__('Global:'); ?></p>
+
+<p><?= Yii::$service->page->translate->__('1. PayPal'); ?></p>
+
+<p><?= Yii::$service->page->translate->__('1) Login To Your Account or use Credit Card Express.'); ?><br>
+<?= Yii::$service->page->translate->__('2) Enter your Card Details, the order will be shipped to your PayPal address. And click "Submit".'); ?><br>
+<?= Yii::$service->page->translate->__('3) Your Payment will be processed and a receipt will be sent to your email inbox.'); ?></p>
+
+<p><?= Yii::$service->page->translate->__('2. Credit Card'); ?></p>
+
+<p>	<?= Yii::$service->page->translate->__('1) Choose your shipping address OR create a new one.'); ?><br>
+	<?= Yii::$service->page->translate->__('2) Enter your Card Details and click "Submit".'); ?><br>
+	<?= Yii::$service->page->translate->__('3) Your Payment will be processed and a receipt will be sent to your email inbox.'); ?></p>
