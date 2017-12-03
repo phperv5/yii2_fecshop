4228e0e1f1bb91c73d753c548ec6b7f349ae3aec
USER-20150613FK\Administrator <jerry@2bai.com.cn>
2017-12-03 18:40:46 +0800

    commit

---
+++ apphtml5/theme/terry/theme01/widgets/header.php
@@ -0,0 +1,28 @@
+<div class="header">
+
+    <div class="hd_logo"><a href="http://m.UOBDII.com"></a></div>
+    <div class="hd_sch">
+
+        <form name="formSearch" method="get" onsubmit="javascript:return CheckFormSiteSearch();" action="/search/">
+            <input name="q" type="text" id="q" class="kw" maxlength="50"
+                   onfocus="javascript:if (this.value=='Lonsdor K518ISE Key Programmer') this.value='';this.className='kwfocus';"
+                   onblur="javascript:if (this.value=='') this.value='Lonsdor K518ISE Key Programmer';"
+                   value="Lonsdor K518ISE Key Programmer">
+            <input type="submit" value="" class="go iconfont icon-search" title="Search">
+        </form>
+    </div>
+
+
+    <div class="hd_nav">
+        <a href="/members/" target="_top" rel="nofollow">
+            <div class="hdn_signin iconfont icon-people" id="hdn_signin"></div>
+        </a>
+        <a href="/app/order.asp" target="_top" rel="nofollow">
+            <div class="hdn_cart iconfont icon-cart">
+                <div class="hdn_my_cart button button--sacnite button--round-l" id="hdn_cart_num" style="display:none;">
+                    <span></span></div>
+            </div>
+        </a>
+    </div>
+    <div class="clear"></div>
+</div>
