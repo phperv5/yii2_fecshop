29263aef5226cb7e7cc7eda0eaa5324d79c58cfb
iwisunny <iwisunny@gmail.com>
2017-12-18 14:04:21 +0800

    修改样式

--- apphtml5/theme/terry/theme01/widgets/header.php
+++ apphtml5/theme/terry/theme01/widgets/header.php
@@ -2,14 +2,14 @@
 <input type="hidden" class="logoutUrl" value="<?= $logoutUrl ?>" />
 <header class="new-header j-headerWarp">
     <div class="new-header-inner">
-        <div class="new-return j-common-back com-ripple-btn"><i class="ic-md ic-return-md"></i></div>
-        <div class="new-head-category j-headCategoryBtn com-ripple-btn"><i class="ic-md ic-menu-md"></i></div>
+        <div class="new-return j-common-back com-ripple-btn"><i class="common-ic-md ic-return-md"></i></div>
+        <div class="new-head-category j-headCategoryBtn com-ripple-btn"><i class="common-ic-md ic-menu-md"></i></div>
         <div class="head-right-Warp">
             <div class="new-head-search com-ripple-btn" id="J_searchBtn">
-                <i class="ic-md ic-search-md"></i>
+                <i class="common-ic-md common-ic-search-md"></i>
             </div>
             <div class="new-head-cart com-ripple-href">
-                <a href="<?= Yii::$service->url->getUrl('checkout/cart') ?>" rel="nofollow" id="cartnum" class="cart"><i class="ic-md ic-shoppingcart-md"></i></a>
+                <a href="<?= Yii::$service->url->getUrl('checkout/cart') ?>" rel="nofollow" id="cartnum" class="cart"><i class="common-ic-md common-ic-shoppingcart-md"></i></a>
             </div>
         </div>
     </div>
