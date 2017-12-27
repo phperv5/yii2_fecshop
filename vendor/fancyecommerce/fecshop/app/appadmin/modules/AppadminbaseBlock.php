d27489c1d20e4c7cfc3f79339f4879fb3b19cfa3
phperv5 <312043814@qq.com>
2017-12-27 14:10:04 +0800

    修改删除

--- vendor/fancyecommerce/fecshop/app/appadmin/modules/AppadminbaseBlock.php
+++ vendor/fancyecommerce/fecshop/app/appadmin/modules/AppadminbaseBlock.php
@@ -582,7 +582,7 @@ class AppadminbaseBlock extends Object
             }
             $str .= '<td class="center">
 						<a title="编辑" target="dialog" class="btnEdit" mask="true" drawable="true" width="1000" height="580" href="'.$this->_editUrl.'?'.$this->_primaryKey.'='.$one[$this->_primaryKey].'" >编辑</a>
-						<a title="删除" target="ajaxTodo" href="'.$this->_editCategoryUrl.'?'.$csrfString.'&'.$this->_primaryKey.'='.$one[$this->_primaryKey].'" class="btnDel">删除</a>
+						<a title="删除" target="ajaxTodo" href="'.$this->_deleteUrl.'?'.$csrfString.'&'.$this->_primaryKey.'='.$one[$this->_primaryKey].'" class="btnDel">删除</a>
 					</td>';
             $str .= '</tr>';
         }
