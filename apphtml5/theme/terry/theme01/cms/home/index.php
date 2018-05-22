c0beb4b25929e3ca43f8575307d90f1bfd1d5ca5
DESKTOP-F7ILDG9\撞见、 <312043814@qq.com>
2018-05-22 16:17:43 +0800

    添加广告

--- apphtml5/theme/terry/theme01/cms/home/index.php
+++ apphtml5/theme/terry/theme01/cms/home/index.php
@@ -1,46 +1,20 @@
 
 <!-- App root element -->
 <div id="app" class="app">
-    <header id="ms-header" class="ms-header">
-        <div id="header-toolbar" class="header-toolbar header-shadow white-bg">
-          <span role="button" tabindex="-1" id="ms-drawer" class="ic-wp" onclick="sidebar.toggle()">
-            <i class="ic-md ic-drawer-md"></i>
-          </span>
-            <div id="ms-logo" class="ic-wp">
-                <div class="i-amphtml-element i-amphtml-layout-fixed i-amphtml-layout-size-defined i-amphtml-layout" style="width: 99px; height: 24px;">
-                    <img class="i-amphtml-fill-content i-amphtml-replaced-content" src="https://ae01.alicdn.com/kf/HTB10eZQQpXXXXXRXVXX760XFXXXO.png">
-                </div>
-                <h1>Aliexpress</h1>
-            </div>
-            <span role="button" tabindex="-1" id="ms-search" class="ic-wp">
-            <i class="ic-md ic-search-md"></i>
-          </span>
-            <a href="cart.html" id="ms-cart" class="ic-wp">
-                <div></div>
-                <i class="ic-md ic-shoppingcart-md"></i>
-            </a>
-        </div>
-    </header>
+
     <div class="page-content index-content">
         <!-- Slider container -->
         <div class="swiper-container index-swiper">
             <div class="swiper-pagination"></div>
             <div class="swiper-wrapper">
+                <?php
+                $middle_banner = Yii::$service->product->banner->getList('middle_position',4);
+                foreach($middle_banner as $v):
+                ?>
                 <div class="swiper-slide">
-                    <a class="" href="details.html"><img class="img-response" src="./img/HTB1tIEHdqmWBuNjy1Xa760CbXXaa.png" /></a>
+                    <a class="" href="<?= $v['url'] ?>"><img class="img-response" src="<?= $v['banner_url'] ?>"  alt="<?= $v['title'] ?>" /></a>
                 </div>
-                <div class="swiper-slide">
-                    <a class="" href="details.html"><img class="img-response" src="./img/HTB1brpBbXooBKNjSZFP760a2XXa0.png" /></a>
-                </div>
-                <div class="swiper-slide">
-                    <a class="" href="details.html"><img class="img-response" src="./img/HTB1brpBbXooBKNjSZFP760a2XXa0.png" /></a>
-                </div>
-                <div class="swiper-slide">
-                    <a class="" href="details.html"><img class="img-response" src="./img/HTB1brpBbXooBKNjSZFP760a2XXa0.png" /></a>
-                </div>
-                <div class="swiper-slide">
-                    <a class="" href="details.html"><img class="img-response" src="./img/HTB1brpBbXooBKNjSZFP760a2XXa0.png" /></a>
-                </div>
+                <?php endforeach;?>
             </div>
         </div>
         <h3 class="big-sale-title ellipsis" data-role="#DEB055">FIND DEALS BY CATEGORY</h3>
--- apphtml5/theme/terry/theme01/widgets/header.php
+++ apphtml5/theme/terry/theme01/widgets/header.php
@@ -1,64 +1,20 @@
-<input type="hidden" class="currentBaseUrl" value="<?= $currentBaseUrl ?>" />
-<input type="hidden" class="logoutUrl" value="<?= $logoutUrl ?>" />
-<input type="hidden" class="logoutStr" value="<?= Yii::$service->page->translate->__('Logout'); ?>" />
-<input type="hidden" class="welcome_str" value="<?= Yii::$service->page->translate->__('Welcome!'); ?>" />
-<div class="hd_wrapper">
-    <div class="hd_wrap_fun">
-        <div class="hd_wr_f_nav" id="hd_f_signin">
-            <div class="login-text t_r">
-            <a href="<?= Yii::$service->url->getUrl('customer/account/login') ?>" class="link_green" rel="nofollow"><b>Sign In</b></a>
-            <span class="gray px11">or</span>
-            <a href="<?= Yii::$service->url->getUrl('customer/account/register'); ?>" class="link_green" rel="nofollow"><b>Register</b></a>
+<header id="ms-header" class="ms-header">
+    <div id="header-toolbar" class="header-toolbar header-shadow white-bg">
+          <span role="button" tabindex="-1" id="ms-drawer" class="ic-wp" onclick="sidebar.toggle()">
+            <i class="ic-md ic-drawer-md"></i>
+          </span>
+        <div id="ms-logo" class="ic-wp">
+            <div class="i-amphtml-element i-amphtml-layout-fixed i-amphtml-layout-size-defined i-amphtml-layout" style="width: 99px; height: 24px;">
+                <img class="i-amphtml-fill-content i-amphtml-replaced-content" src="https://ae01.alicdn.com/kf/HTB10eZQQpXXXXXRXVXX760XFXXXO.png">
             </div>
+            <h1>Aliexpress</h1>
         </div>
-        <div class="hd_wr_f_bar">
-            <div class="hd_wr_f_my">
-                <div class="hd_wr_nav"><a href="<?= Yii::$service->url->getUrl('customer/order') ?>" rel="nofollow"><?= Yii::$service->page->translate->__('My Account'); ?></a>
-                    <ul>
-                        <a href="<?= Yii::$service->url->getUrl('customer/order') ?>"
-                           rel="nofollow"><?= Yii::$service->page->translate->__('My Orders'); ?><span
-                                    id="myaccount_num_orders"></span></a>
-                        <a href="<?= Yii::$service->url->getUrl('customer/productreview') ?>" rel="nofollow"><?= Yii::$service->page->translate->__('My Review'); ?><span id="myaccount_num_orders"></span></a>
-                        <a id="myaccount_myfavorites" href="<?= Yii::$service->url->getUrl('customer/productfavorite') ?>" rel="nofollow"><?= Yii::$service->page->translate->__('My Favorites'); ?><span id="myaccount_num_orders"></span></a>
-                    </ul>
-                </div>
-            </div>
-            <div class="hd_wr_f_help">
-                <div class="hd_wr_nav"><a href="support/support.html">Help</a>
-                    <ul>
-                        <a href="<?= Yii::$service->url->getUrl('help') ?>">Help Center</a>
-                        <a href="<?= Yii::$service->url->getUrl('freed back') ?>">Feed Back</a>
-                    </ul>
-                </div>
-            </div>
-            <div class="hd_wr_f_help">
-            <div class="hd_wr_nav">
-                        <a class="current_currency" style="color: #039;padding-left:10px"><b><?= $currency['code'] ?></b></a>
-                        <ul class="currency_list">
-                            <?php foreach($currencys as $c):    ?>
-                                <a rel="<?= $c['code'] ?>"><?= $c['code'] ?></a>
-                            <?php endforeach; ?>
-                        </ul>
-            </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="header" id="WebPageTop">
-    <div class="hd_logo"><a href="<?= $homeUrl ?>" target="_top"><h1></h1></a></div>
-    <?= Yii::$service->page->widget->render('topsearch',$this); ?>
-    <div class="hd_cart">
-        <a href="<?= Yii::$service->url->getUrl('checkout/cart') ?>" target="_top" rel="nofollow" id="hd_mycart">
-            <span class="mycart-text">My Shopping Cart</span>
+        <span role="button" tabindex="-1" id="ms-search" class="ic-wp">
+            <i class="ic-md ic-search-md"></i>
+          </span>
+        <a href="cart.html" id="ms-cart" class="ic-wp">
+            <div></div>
+            <i class="ic-md ic-shoppingcart-md"></i>
         </a>
     </div>
-    <div class="hd_pop_kw">
-        <?php
-            $keywords = Yii::$service->product->keywords->getKeywordsList(1);
-            foreach($keywords as $v):
-        ?>
-          <a href="<?php if(isset($v['url']) && !empty($v['url'])) echo $v['url'];else echo  Yii::$service->url->getUrl('catalogsearch/index?q='.$v['keywords']);?>"><?= $v['keywords'];?></a>
-        <?php endforeach;?>
-    </div>
-    <div class="clear">
-    </div>
+</header>
