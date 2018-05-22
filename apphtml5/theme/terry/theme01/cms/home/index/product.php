8443a0a89ba78e9f2cb962317da2f56ad343e4dc
DESKTOP-F7ILDG9\撞见、 <312043814@qq.com>
2018-05-22 19:34:27 +0800

    修改

--- apphtml5/theme/terry/theme01/cms/home/index/product.php
+++ apphtml5/theme/terry/theme01/cms/home/index/product.php
@@ -1,31 +1,27 @@
-<?php
-/**
- * FecShop file.
- *
- * @link http://www.fecshop.com/
- * @copyright Copyright (c) 2016 FecShop Software LLC
- * @license http://www.fecshop.com/license/
- */
-?>
 <?php  if(is_array($parentThis['products']) && !empty($parentThis['products'])): ?>
-    <?php foreach($parentThis['products'] as $item=>$product): ?>
-<div class="hm_pro_grid <?php if($item>=5):?> hm_pro_gr_col_2 <?php  endif;  ?>?>">
-                <div class="hm_pro_gr_item <?php if(!$item || $item==5):?>hm_pro_gr_item_noad_left<?php  endif;  ?>">
-                    <div class="hm_progrd_photo"><a href="<?= $product['url'] ?>"><img src="<?= Yii::$service->product->image->getResize($product['image'],[200,200],false) ?>" width="180" height="180" border="0" hspace="0" vspace="0" alt="<?= $product['name'] ?>" align="absmiddle" /></a></div>
-                    <div class="hm_progrd_name"><a href="<?= $product['url'] ?>" title="<?= $product['name'] ?>"><?= $product['name'] ?></a></div>
-                    <div class="hm_progrd_price" id="ProPriDisp_h_dp_51561">
-                        <?php
-                        $config = [
-                            'class' 		=> 'fecshop\app\appfront\modules\Catalog\block\category\Price',
-                            'view'  		=> 'cms/home/index/price.php',
-                            'price' 		=> $product['price'],
-                            'special_price' => $product['special_price'],
-                        ];
-                        echo Yii::$service->page->widget->renderContent('category_newArrivals_price',$config);
-                        ?>
+<div class="qp-list">
+    <div class="row">
+        <div layout="fill" id="just-for-you" class="i-amphtml-layout-fill i-amphtml-layout-size-defined i-amphtml-layout">
+            <div class="i-amphtml-fill-content i-amphtml-replaced-content" role="list">
+            <?php foreach($parentThis['products'] as $product): ?>
+                <a href="details.html" class="grid-qp" target="_top" role="listitem">
+                    <div class="product radius shadow">
+                        <div class="img">
+                            <div layout="responsive" class="img-box i-amphtml-element i-amphtml-layout-responsive i-amphtml-layout-size-defined i-amphtml-layout">
+                                <span style="display: block; padding-top: 100%;"></span>
+                                <img alt="<?= $product['name'] ?>" class="i-amphtml-fill-content i-amphtml-replaced-content" src="<?= Yii::$service->product->image->getResize($product['image'],[285,434],false) ?>">
+                            </div>
+                        </div>
+                        <div class="none-img">
+                            <div class="price">
+                                US $2.99
+                            </div>
+                        </div>
                     </div>
-<!--                    <div class="hm_progrd_rate"><div class="pro_g_r4_prate"><div class="rate_star_w75"><div class="rate_star_w75_bg"><div class="rate_star_w75_vw" style="width:72px;"></div></div></div><div class="rate_star_w75_tx">(<a href="reviews/pro51561.html" target="_blank">55</a>)</div></div></div>-->
-                </div>
+                </a>
+            <?php  endforeach;  ?>
+            </div>
+        </div>
+    </div>
 </div>
-    <?php  endforeach;  ?>
 <?php  endif;  ?>
