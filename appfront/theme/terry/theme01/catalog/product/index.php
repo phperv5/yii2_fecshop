0a9c07cc3e1598644914e3670356604211373699
DESKTOP-F7ILDG9\撞见、 <312043814@qq.com>
2018-06-03 11:06:38 +0800

    12

--- appfront/theme/terry/theme01/catalog/product/index.php
+++ appfront/theme/terry/theme01/catalog/product/index.php
@@ -32,9 +32,7 @@
                     <span id="num_pro_sold_51561"></span>
                 </div>
                 <div class="pro_ch_bf_rate" id="pro_rate_51561">
-                    <div class="pro_ch_bf_rate_bg">
 
-                    </div>
                     <div class="pro_ch_bf_rate_tx"><?= $reviw_rate_star_average ?> stars, <a
                                 href="<?= Yii::$service->url->getUrl('catalog/reviewproduct/lists', ['spu' => $spu, '_id' => $_id]); ?>"
                                 target="_blank"><?= $review_count ?> reviews.</a></div>
